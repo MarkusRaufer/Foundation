e4028e028153e37aa6ac2a6236f98fb39a719623
Markus Raufer <markus.raufer@gmail.com>
2022-02-19 23:06:56 +0100

    refactor ToIso8601Period consider negative TimeSpan

---
+++ Foundation6.Tests/TimeSpanTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foundation
+{
+    [TestFixture]
+    public class TimeSpanTests
+    {
+        [Test]
+        public void ToIso8601PeriodString_Should_ReturnIso8601String_When_NegativeTimeSpan()
+        {
+            var start = new DateTime(2015, 1, 1);
+            var end = new DateTime(2016, 2, 3, 4, 5, 6, 789);
+            var ts = start - end;
+
+            var iso = ts.ToIso8601Period();
+            Assert.AreEqual("-P398DT4H5M6S789", iso);
+        }
+
+        [Test]
+        public void ToIso8601PeriodString_Should_ReturnIso8601String_When_PositiveTimeSpan()
+        {
+            var start = new DateTime(2015, 1, 1);
+            var end = new DateTime(2016, 2, 3, 4, 5, 6, 789);
+            var ts = end - start;
+
+            var iso = ts.ToIso8601Period();
+            Assert.AreEqual("P398DT4H5M6S789F", iso);
+        }
+    }
+}
--- Foundation6/TimeSpanExtensions.cs
+++ Foundation6/TimeSpanExtensions.cs
@@ -9,21 +9,22 @@ public static class TimeSpanExtensions
         return (int)Math.Ceiling(duration.TotalDays);
     }
 
-    public static string ToIso8601PeriodString(this TimeSpan duration)
+    public static string ToIso8601Period(this TimeSpan duration)
     {
         var sb = new StringBuilder();
         if (TimeSpan.Zero > duration)
             sb.Append('-');
 
         sb.Append('P');
-        if (0 < duration.Days)
+        if (0 != duration.Days)
         {
-            sb.Append(duration.Days);
+            var days = Math.Abs(duration.Days);
+            sb.Append(days);
             sb.Append('D');
         }
 
         var diff = duration - TimeSpan.FromDays(duration.Days);
-        if (TimeSpan.Zero < diff)
+        if (TimeSpan.Zero != diff)
         {
             sb.Append('T');
         }
@@ -32,57 +33,60 @@ public static class TimeSpanExtensions
             return sb.ToString();
         }
 
-
-        if (0 < duration.Hours)
+        if (0 != duration.Hours)
         {
-            sb.Append(duration.Hours);
+            var hours = Math.Abs(duration.Hours);
+            sb.Append(hours);
             sb.Append('H');
         }
-        if (0 < duration.Minutes)
+        if (0 != duration.Minutes)
         {
-            sb.Append(duration.Minutes);
+            var minutes = Math.Abs(duration.Minutes);
+            sb.Append(minutes);
             sb.Append('M');
         }
-        if (0 < duration.Seconds)
+        if (0 != duration.Seconds)
         {
-            sb.Append(duration.Seconds);
+            var seconds = Math.Abs(duration.Seconds);
+            sb.Append(seconds);
             sb.Append('S');
         }
-        if (0 < duration.Milliseconds)
+        if (0 != duration.Milliseconds)
         {
-            sb.Append(duration.Milliseconds);
+            var milliseconds = Math.Abs(duration.Milliseconds);
+            sb.Append(milliseconds);
             sb.Append('F');
         }
 
         return sb.ToString();
     }
 
-    public static IEnumerable<string> ToIso8601PeriodStringParts(this TimeSpan duration)
+    public static IEnumerable<string> ToIso8601PeriodParts(this TimeSpan duration)
     {
         var sb = new StringBuilder();
         if (TimeSpan.Zero > duration)
             yield return "-";
 
         yield return "P";
-        if (0 < duration.Days)
-            yield return $"{duration.Days}D";
+        if (0 != duration.Days)
+            yield return $"{Math.Abs(duration.Days)}D";
 
         var diff = duration - TimeSpan.FromDays(duration.Days);
-        if (TimeSpan.Zero < diff)
+        if (TimeSpan.Zero != diff)
             yield return "T";
         else
             yield break;
 
-        if (0 < duration.Hours)
-            yield return $"{duration.Hours}H";
+        if (0 != duration.Hours)
+            yield return $"{Math.Abs(duration.Hours)}H";
 
-        if (0 < duration.Minutes)
-            yield return $"{duration.Minutes}M";
+        if (0 != duration.Minutes)
+            yield return $"{Math.Abs(duration.Minutes)}M";
 
-        if (0 < duration.Seconds)
-            yield return $"{duration.Seconds}S";
+        if (0 != duration.Seconds)
+            yield return $"{Math.Abs(duration.Seconds)}S";
 
-        if (0 < duration.Milliseconds)
-            yield return $"{duration.Milliseconds}F";
+        if (0 != duration.Milliseconds)
+            yield return $"{Math.Abs(duration.Milliseconds)}F";
     }
 }
