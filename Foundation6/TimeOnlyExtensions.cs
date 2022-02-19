a47aefd851f6948293288ddd749ed0fc4801ab66
Markus Raufer <markus.raufer@gmail.com>
2022-02-19 23:08:30 +0100

    move method to TimeOnlyExtensions

--- Foundation6/TimeOnlyExtensions.cs
+++ Foundation6/TimeOnlyExtensions.cs
@@ -3,5 +3,10 @@
 public static class TimeOnlyExtensions
 {
     public static TimeOnly Subtract(this TimeOnly time, TimeSpan span) => new(time.Ticks - span.Ticks);
+
+    public static TimeSpan ToTimeSpan(this TimeOnly time)
+    {
+        return new TimeSpan(0, time.Hour, time.Minute, time.Second, time.Millisecond);
+    }
 }
 
