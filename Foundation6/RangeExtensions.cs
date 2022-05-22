cbc3e64585fc4877b3117059b5a6e2421d533760
Markus Raufer <markus.raufer@gmail.com>
2022-05-22 03:55:51 +0200

    add RangeExtensions

---
+++ Foundation6/RangeExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Foundation
+{
+    public static class RangeExtensions
+    {
+        /// <summary>
+        /// Checks if value is within Start and End. Index from end are not supported.
+        /// </summary>
+        /// <param name="range"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsInRange(this System.Range range, int value)
+        {
+            if (range.Start.Value > value) return false;
+
+            if (range.End.IsFromEnd) 
+                return 0 == range.End.Value;
+
+            return range.End.Value >= value;
+        }
+    }
+}
