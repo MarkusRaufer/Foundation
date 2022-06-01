3ee128e36d3333fb24882c6300a1b0ed8c439198
Markus Raufer <markus.raufer@gmail.com>
2022-06-02 00:46:44 +0200

    add EquatableHashSet
    refactor EquatableCollection

--- Foundation6/ComponentModel/EquatableCollection.cs
+++ Foundation6/ComponentModel/EquatableCollection.cs
@@ -3,32 +3,53 @@
 using Foundation.Collections.Generic;
 using System.Collections;
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization;
 
+[Serializable]
 public class EquatableCollection<T>
     : ICollection<T>
+    , ISerializable
     , IEquatable<EquatableCollection<T>>
 {
     private readonly ICollection<T> _collection;
     private int _hashCode;
 
-    public EquatableCollection(bool allowDuplicates = true) : this(new List<T>(), allowDuplicates)
+    public EquatableCollection() : this(new List<T>())
     {
     }
 
-    public EquatableCollection([DisallowNull] ICollection<T> collection, bool allowDuplicates = true)
+    public EquatableCollection([DisallowNull] ICollection<T> collection)
     {
         _collection = collection.ThrowIfNull();
-        AllowDuplicates = allowDuplicates;
+        _hashCode = 0 == collection.Count ? 0 : HashCode.FromObjects(_collection);
 
-        _hashCode = HashCode.FromObjects(_collection);
+        CollectionChanged = new Event<Action<CollectionEvent<T>>>();
+    }
 
+    public EquatableCollection(int capacity) : this(new List<T>(capacity))
+    {
+    }
+
+    public EquatableCollection(SerializationInfo info, StreamingContext context)
+    {
+        if (info.GetValue(nameof(_collection), typeof(List<T>)) is List<T> collection)
+        {
+            _collection = collection;
+            _hashCode = HashCode.FromObjects(_collection);
+        }
+        else
+        {
+            _hashCode = 0;
+            _collection = new List<T>();
+        }
         CollectionChanged = new Event<Action<CollectionEvent<T>>>();
     }
 
     public void Add(T item)
     {
         item.ThrowIfNull();
-        if(!AllowDuplicates && _collection.Contains(item)) throw new ArgumentException($"{item} exists");
+        
+        _collection.Add(item);
 
         var hcb = HashCode.CreateBuilder();
         hcb.AddHashCode(_hashCode);
@@ -38,8 +59,6 @@ public class EquatableCollection<T>
 
         CollectionChanged.Publish(new { State = CollectionChangedState.ElementAdded, Element = item });
     }
-
-    public bool AllowDuplicates { get; }
 
     public void Clear()
     {
@@ -62,7 +81,10 @@ public class EquatableCollection<T>
 
     public bool Equals(EquatableCollection<T>? other)
     {
-        return null != other && _collection.IsEqualTo(other._collection);
+        if (other is null) return false;
+        if (_hashCode != other._hashCode) return false;
+
+        return _collection.IsEqualTo(other._collection);
     }
 
     IEnumerator IEnumerable.GetEnumerator() => _collection.GetEnumerator();
@@ -71,15 +93,23 @@ public class EquatableCollection<T>
 
     public override int GetHashCode() => _hashCode;
 
+    public void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        info.AddValue(nameof(_collection), _collection);
+    }
+
     public bool Remove(T item)
     {
         item.ThrowIfNull();
 
         var removed = _collection.Remove(item);
 
-        if (removed) _hashCode = HashCode.FromObjects(_collection);
+        if (removed)
+        {
+            _hashCode = HashCode.FromObjects(_collection);
 
-        CollectionChanged.Publish(new { State = CollectionChangedState.ElementRemoved, Element = item });
+            CollectionChanged.Publish(new { State = CollectionChangedState.ElementRemoved, Element = item });
+        }
         return removed;
     }
 }
---
+++ Foundation6/ComponentModel/EquatableHashSet.cs
@@ -0,0 +1,103 @@
+﻿namespace Foundation.Collections.ComponentModel;
+
+using Foundation.Collections.Generic;
+using System.Runtime.Serialization;
+
+[Serializable]
+public class EquatableHashSet<T>
+    : HashSet<T>
+    , ISerializable
+    , IEquatable<EquatableHashSet<T>>
+{
+    private int _hashCode;
+
+    public EquatableHashSet() : base()
+    {
+        _hashCode = 0;
+        CollectionChanged = new Event<Action<CollectionEvent<T>>>();
+    }
+
+    public EquatableHashSet(IEnumerable<T> collection) : base(collection)
+    {
+        _hashCode = HashCode.FromObjects(collection);
+        CollectionChanged = new Event<Action<CollectionEvent<T>>>();
+    }
+
+    public EquatableHashSet(IEqualityComparer<T>? comparer) :base(comparer)
+    {
+        _hashCode = 0;
+        CollectionChanged = new Event<Action<CollectionEvent<T>>>();
+    }
+
+    public EquatableHashSet(int capacity) : base(capacity)
+    {
+        _hashCode = 0;
+        CollectionChanged = new Event<Action<CollectionEvent<T>>>();
+    }
+
+    public EquatableHashSet(IEnumerable<T> collection, IEqualityComparer<T>? comparer) : base(collection, comparer)
+    {
+        _hashCode = HashCode.FromObjects(collection);
+        CollectionChanged = new Event<Action<CollectionEvent<T>>>();
+    }
+
+    public EquatableHashSet(int capacity, IEqualityComparer<T>? comparer) : base(capacity, comparer)
+    {
+        _hashCode = 0;
+        CollectionChanged = new Event<Action<CollectionEvent<T>>>();
+    }
+
+    public EquatableHashSet(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+        _hashCode = HashCode.FromObjects(this);
+        CollectionChanged = new Event<Action<CollectionEvent<T>>>();
+    }
+
+    public new void Add(T item)
+    {
+        if (base.Add(item.ThrowIfNull()))
+        {
+            var hcb = HashCode.CreateBuilder();
+            hcb.AddHashCode(_hashCode);
+            hcb.AddObject(item);
+
+            _hashCode = hcb.GetHashCode();
+
+            CollectionChanged.Publish(new { State = CollectionChangedState.ElementAdded, Element = item });
+        }
+    }
+
+    public new void Clear()
+    {
+        base.Clear();
+        _hashCode = 0;
+        CollectionChanged.Publish(new { State = CollectionChangedState.CollectionCleared });
+    }
+
+    public Event<Action<CollectionEvent<T>>> CollectionChanged { get; private set; }
+
+    public override bool Equals(object? obj) => obj is EquatableHashSet<T> other && Equals(other);
+
+    public bool Equals(EquatableHashSet<T>? other)
+    {
+        if (null == other) return false;
+        if(_hashCode != other._hashCode) return false;
+
+        return this.IsEqualTo(other);
+    }
+
+    public override int GetHashCode() => _hashCode;
+
+    public new bool Remove(T item)
+    {
+        item.ThrowIfNull();
+
+        var removed = base.Remove(item);
+        if (removed)
+        {
+            _hashCode = HashCode.FromObjects(this);
+            CollectionChanged.Publish(new { State = CollectionChangedState.ElementRemoved, Element = item });
+        }
+        return removed;
+    }
+}
