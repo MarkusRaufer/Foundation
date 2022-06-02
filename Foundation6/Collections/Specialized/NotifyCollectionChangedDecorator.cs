6733c67dda355450ead2351b7cc92aab2acc4e0c
Markus Raufer <markus.raufer@gmail.com>
2022-06-02 11:19:16 +0200

    add NotifyCollectionChangedDecorator

---
+++ Foundation6/Collections/Specialized/NotifyCollectionChangedDecorator.cs
@@ -0,0 +1,24 @@
+﻿using Foundation.ComponentModel;
+using System.Collections.Specialized;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Foundation.Collections.Specialized
+{
+    public class NotifyCollectionChangedDecorator<T> : INotifyCollectionChanged
+    {
+        public event NotifyCollectionChangedEventHandler? CollectionChanged;
+
+        private readonly ICollectionChanged<T> _collectionChanged;
+
+        public NotifyCollectionChangedDecorator([DisallowNull] ICollectionChanged<T> collectionChanged)
+        {
+            _collectionChanged = collectionChanged.ThrowIfNull();
+            _collectionChanged.CollectionChanged.Subscribe(OnCollectionChanged);
+        }
+
+        private void OnCollectionChanged(CollectionEvent<T> @event)
+        {
+            CollectionChanged?.Invoke(_collectionChanged, @event.ToNotifyCollectionChangedEventArgs());
+        }
+    }
+}
