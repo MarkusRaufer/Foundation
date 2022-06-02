4c01390e3eb74d0aab59f421cac2356f4ee507a1
Markus Raufer <markus.raufer@gmail.com>
2022-06-02 11:19:36 +0200

    add CollectionEventExtensions

---
+++ Foundation6/Collections/Specialized/CollectionEventExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Foundation.ComponentModel;
+using System.Collections.Specialized;
+
+namespace Foundation.Collections.Specialized
+{
+    public static class CollectionEventExtensions
+    {
+        public static NotifyCollectionChangedEventArgs ToNotifyCollectionChangedEventArgs<T>(this CollectionEvent<T> @event)
+        {
+            return @event.State switch
+            {
+                CollectionChangedState.CollectionCleared => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset),
+                CollectionChangedState.ElementAdded => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, @event.Element),
+                CollectionChangedState.ElementRemoved => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, @event.Element),
+                CollectionChangedState.ElementReplaced => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, @event.Element),
+                _ => throw new NotImplementedException()
+            };
+        }
+    }
+}
