6d979da8652ef6dd46f047c781d503357ffb2f33
Markus Raufer <markus.raufer@gmail.com>
2022-06-14 08:44:48 +0200

    refactor MinMax

--- Foundation6/MinMax.cs
+++ Foundation6/MinMax.cs
@@ -5,6 +5,14 @@ using System.Diagnostics.CodeAnalysis;
 public static class MinMax
 {
     public static MinMax<T> New<T>([DisallowNull] T min, [DisallowNull] T max) where T : notnull => new(min, max);
+
+    public static MinMax<int> New(System.Range range)
+    {
+        if (range.Start.IsFromEnd) throw new ArgumentException($"{range.Start}.IsFromEnd is not allowed");
+        if (range.End.IsFromEnd) throw new ArgumentException($"{range.End}.IsFromEnd is not allowed");
+
+        return new MinMax<int>(range.Start.Value, range.End.Value);
+    }
 }
 
 public record struct MinMax<T>([DisallowNull] T Min, [DisallowNull] T Max) where T : notnull;
---
+++ Foundation6/MinMaxExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Foundation
+{
+    public static class MinMaxExtensions
+    {
+        public static System.Range ToRange(this MinMax<int> minMax)
+        {
+            return new System.Range(new Index(minMax.Min), new Index(minMax.Max));
+        }
+    }
+}
