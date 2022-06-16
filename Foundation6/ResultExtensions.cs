9c822c0c958a9ccec759a20959a66eb4ab2c23cf
Markus Raufer <markus.raufer@gmail.com>
2022-06-16 15:21:22 +0200

    add ResultExtensions

---
+++ Foundation6/ResultExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Foundation
+{
+    public static class ResultExtensions
+    {
+        /// <summary>
+        /// Converts a result to an optional.
+        /// </summary>
+        /// <typeparam name="TOk"></typeparam>
+        /// <typeparam name="TError"></typeparam>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public static Opt<TOk> ToOpt<TOk, TError>(this Result<TOk, TError> result) 
+            => result.IsOk ? Opt.Some(result.Ok) : Opt.None<TOk>();
+    }
+}
