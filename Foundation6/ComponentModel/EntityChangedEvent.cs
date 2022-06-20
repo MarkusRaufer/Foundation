b3c8af6e098c841601fb443e16503eba2f87d429
Markus Raufer <markus.raufer@gmail.com>
2022-06-20 22:40:58 +0200

    refactor EntityChangedEvent

--- Foundation6/ComponentModel/EntityChangedEvent.cs
+++ Foundation6/ComponentModel/EntityChangedEvent.cs
@@ -1,68 +1,12 @@
 ﻿namespace Foundation.ComponentModel;
 
-using System.Diagnostics.CodeAnalysis;
+public record class EntityChangedEvent<TId>(KeyValue<string, TId> Identifier, PropertyChangedEvent PropertyChanged)
+    : IIndexedIdentifiable<string, TId>
+    , IPropertyChangedContainer;
 
-public class EntityChangedEvent<TId>
-    : IndexedIdentifiable<string, TId>
-    , IIdentifiable<TId>
-    , IPropertyChangedContainer
-    , IEquatable<EntityChangedEvent<TId>>
-    where TId : notnull
-{
-    public EntityChangedEvent(KeyValue<string, TId> identifier, PropertyChangedEvent propertyChanged) 
-        : base(identifier)
-    {
-        PropertyChanged = propertyChanged.ThrowIfNull();
-    }
-
-    public TId Id => Identifier.Value;
-
-    [NotNull]
-    public PropertyChangedEvent PropertyChanged { get; }
-
-    public override bool Equals(object? obj) => Equals(obj as EntityChangedEvent<TId>);
-
-    public bool Equals(EntityChangedEvent<TId>? other)
-    {
-        return null != other && base.Equals(other) && PropertyChanged.Equals(other.PropertyChanged);
-    }
-
-    public override int GetHashCode() => System.HashCode.Combine(Identifier, PropertyChanged);
-
-    public override string ToString() => $"{Identifier} => {PropertyChanged}";
-}
-
-public class EntityChangedEvent<TObjectType, TId>
-    : EntityChangedEvent<TId>
-    , ITypedObject<TObjectType>
-    , IEquatable<EntityChangedEvent<TObjectType, TId>>
-    where TId : notnull
-    where TObjectType : notnull
-{
-    public EntityChangedEvent(
-        TObjectType objectType,
-        KeyValue<string, TId> identifier,
-        PropertyChangedEvent propertyChanged)
-        : base(identifier, propertyChanged)
-    {
-        ObjectType = objectType.ThrowIfNull();
-    }
-
-    public override bool Equals(object? obj) => Equals(obj as EntityChangedEvent<TObjectType, TId>);
-
-    public bool Equals(EntityChangedEvent<TObjectType, TId>? other)
-    {
-        return null != other
-            && ObjectType.Equals(other.ObjectType)
-            && base.Equals(other);
-            
-    }
-
-    public override int GetHashCode() => System.HashCode.Combine(ObjectType, Identifier, PropertyChanged);
-
-    [NotNull]
-    public TObjectType ObjectType { get; }
-
-    public override string ToString() => $"{ObjectType}, {Identifier} => {PropertyChanged}";
-}
-
+public record class EntityChangedEvent<TObjectType, TId>(
+    TObjectType ObjectType, 
+    KeyValue<string, TId> Identifier, 
+    PropertyChangedEvent PropertyChanged)
+    : EntityChangedEvent<TId>(Identifier, PropertyChanged)
+    , ITypedObject<TObjectType>;
