157a5cd7ff8e763c0851984a3bdde0e8142df197
Markus Raufer <markus.raufer@gmail.com>
2022-06-20 18:57:14 +0200

    beautify ReadOnlySpanAndMemoryExtensionsBenchmarks

--- Foundation6.Benchmark/ReadOnlySpanAndMemoryExtensionsBenchmarks.cs
+++ Foundation6.Benchmark/ReadOnlySpanAndMemoryExtensionsBenchmarks.cs
@@ -5,8 +5,6 @@ using Foundation.Buffers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 [MemoryDiagnoser]
 public class ReadOnlySpanAndMemoryExtensionsBenchmarks
