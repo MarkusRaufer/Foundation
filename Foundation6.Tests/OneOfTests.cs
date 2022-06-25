f51aac20da4895cf36f9e886dd82bdf4a8a2842e
Markus Raufer <markus.raufer@gmail.com>
2022-06-25 18:50:49 +0200

    refactor: OneOf

--- Foundation6.Tests/OneOfTests.cs
+++ Foundation6.Tests/OneOfTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace Foundation
 {
@@ -6,10 +7,48 @@ namespace Foundation
     public class OneOfTests
     {
         [Test]
-        public void Item2_Should_ReturnSome_When_TypeIsTheScondTypeArgument()
+        public void Invoke_Should_CallTheMatchingAction_When_TypeMatches()
         {
             {
-                int expected = 12;
+                var expected = 12;
+                var sut = new OneOf<int, double>(expected);
+                var value = 0;
+
+                sut.Invoke((int i) => value = i);
+                sut.Invoke((double _) => value = 20);
+
+                Assert.AreEqual(expected, value);
+            }
+            {
+                var expected = 12.3;
+                var sut = new OneOf<int, double>(expected);
+                var value = 0D;
+
+                sut.Invoke((int _) => value = 20D);
+                sut.Invoke((double d) => value = d);
+
+                Assert.AreEqual(expected, value);
+            }
+            {
+                var expected = "myValue";
+                var sut = new OneOf<int, string, double>(expected);
+                var value = "";
+
+                sut.Invoke((int _) => value = "int");
+                sut.Invoke((string s) => value = s);
+                sut.Invoke((double _) => value = "double");
+
+                Assert.AreEqual(expected, value);
+            }
+        }
+
+    
+
+        [Test]
+        public void Item1_Should_ReturnSome_When_TypeIsTheFirstTypeArgument()
+        {
+            {
+                var expected = 12;
                 var sut = new OneOf<int, string>(expected);
 
                 Assert.IsTrue(sut.Item1.IsSome);
@@ -29,10 +68,10 @@ namespace Foundation
         }
 
         [Test]
-        public void Item1_Should_ReturnSome_When_TypeIsTheFirstTypeArgument()
+        public void Item2_Should_ReturnSome_When_TypeIsTheScondTypeArgument()
         {
             {
-                int expected = 12;
+                var expected = 12;
                 var sut = new OneOf<int, string>(expected);
 
                 Assert.IsTrue(sut.Item1.IsSome);
@@ -48,6 +87,35 @@ namespace Foundation
 
                 Assert.IsTrue(sut.Item2.IsSome);
                 Assert.AreEqual(expected, sut.Item2.OrThrow());
+            }
+        }
+
+        [Test]
+        public void Match_Should_ReturnTheResultOfTheMatchingFunc_When_CalledRightFunc()
+        {
+            {
+                var expected = 12;
+                var sut = new OneOf<int, double>(expected);
+
+                var result = sut.Match((int i) => i, (double _) => 20);
+
+                Assert.AreEqual(expected, result);
+            }
+            {
+                var expected = 12.3;
+                var sut = new OneOf<int, double>(expected);
+
+                var result = sut.Match(_ => 20.0, (double d) => d);
+
+                Assert.AreEqual(expected, result);
+            }
+            {
+                var expected = "myValue";
+                var sut = new OneOf<int, string, double>(expected);
+
+                var result = sut.Match((int _) => "int", (string s) => s, (double _) => "double");
+
+                Assert.AreEqual(expected, result);
             }
         }
 
@@ -77,34 +145,67 @@ namespace Foundation
         }
 
         [Test]
-        public void Try_Should_ReturnFalse_When_TypeIsNotTheMatchingType()
+        public void SelectedType_Should_ReturnInt32Type_When_Using2TypeArguments_And_Item1Type_IsInt32()
         {
             var expected = 12;
-            var sut = new OneOf<int, double>(expected);
-            Assert.IsFalse(sut.Try(out double _));
-            Assert.IsFalse(sut.Try(out string _));
+            var sut = new OneOf<int, string>(expected);
+
+            Assert.AreEqual(expected.GetType(), sut.SelectedType);
         }
 
         [Test]
-        public void Try_Should_ReturnTrue_When_TypeIsTheMatchingType()
+        public void SelectedType_Should_ReturnStringType_When_Using2TypeArguments_And_Item2Type_IsString()
+        {
+            var expected = "12";
+            var sut = new OneOf<int, string>(expected);
+
+            Assert.AreEqual(expected.GetType(), sut.SelectedType);
+        }
+
+        [Test]
+        public void SelectedType_Should_ReturnStringType_When_Using4TypeArguments_And_Item1Type_IsString()
+        {
+            var expected = "12";
+            var sut = new OneOf<int, string, DateTime, double>(expected);
+
+            Assert.AreEqual(expected.GetType(), sut.SelectedType);
+        }
+
+        [Test]
+        public void TryGet_Should_ReturnFalse_When_TypeIsNotTheMatchingType()
+        {
+            var expected = 12;
+            var sut = new OneOf<int, double>(expected);
+            Assert.IsFalse(sut.TryGet(out double _));
+            Assert.IsFalse(sut.TryGet(out string _));
+        }
+
+        [Test]
+        public void TryGet_Should_ReturnTrue_When_TypeIsTheMatchingType()
         {
             {
                 var expected = 12;
                 var sut = new OneOf<int, double>(expected);
-                Assert.IsTrue(sut.Try(out int intValue));
+                Assert.IsTrue(sut.TryGet(out int intValue));
                 Assert.AreEqual(expected, intValue);
             }
             {
                 var expected = 12.3;
                 var sut = new OneOf<int, double>(expected);
-                Assert.IsTrue(sut.Try(out double doubleValue));
+                Assert.IsTrue(sut.TryGet(out double doubleValue));
                 Assert.AreEqual(expected, doubleValue);
 
             }
             {
                 var expected = "myValue";
                 var sut = new OneOf<int, string, double>(expected);
-                Assert.IsTrue(sut.Try(out string? stringValue));
+                Assert.IsTrue(sut.TryGet(out string? stringValue));
+                Assert.AreEqual(expected, stringValue);
+            }
+            {
+                var expected = "myValue";
+                var sut = new OneOf<int, string, DateTime, double>(expected);
+                Assert.IsTrue(sut.TryGet(out string? stringValue));
                 Assert.AreEqual(expected, stringValue);
             }
         }
--- Foundation6/OneOf.cs
+++ Foundation6/OneOf.cs
@@ -3,11 +3,11 @@
 using System.Diagnostics.CodeAnalysis;
 
 /// <summary>
-/// Types are mutually exclusive. Just one of the types is set.
+/// This class represent disjunct alternatives of types. Types are mutually exclusive. Just one of the types is set.
 /// </summary>
 /// <typeparam name="T1"></typeparam>
 /// <typeparam name="T2"></typeparam>
-public class OneOf<T1, T2>
+public class OneOf<T1, T2> : IEquatable<OneOf<T1, T2>>
 {
     protected OneOf()
     {
@@ -19,22 +19,60 @@ public class OneOf<T1, T2>
         Item1 = Opt.Some(t1);
 
         OrdinalIndex = 1;
+        SelectedType = typeof(T1);
+        Value = t1!;
     }
 
     public OneOf(T2 t2)
     {
         t2.ThrowIfNull();
         Item2 = Opt.Some(t2);
-
+        
         OrdinalIndex = 2;
+        SelectedType = typeof(T2);
+        Value = t2!;
+    }
+
+    public override bool Equals(object? obj) => Equals(obj as OneOf<T1, T2>);
+
+    public bool Equals(OneOf<T1, T2>? other)
+    {
+        return null != other 
+            && SelectedType!.Equals(other.SelectedType)
+            && Value!.Equals(other.Value);
+    }
+
+    public override int GetHashCode() => System.HashCode.Combine(SelectedType, Value);
+    
+    public void Invoke(Action<T1> action)
+    {
+        if(Value is T1 value) action(value);
+    }
+
+    public void Invoke(Action<T2> action)
+    {
+        if (Value is T2 value) action(value);
     }
 
     public Opt<T1> Item1 { get; }
+
     public Opt<T2> Item2 { get; }
+
+    public TResult Match<TResult>(
+        Func<T1, TResult> t1Projection,
+        Func<T2, TResult> t2Projection)
+    {
+        return Value switch
+        {
+            T1 t1 => t1Projection(t1),
+            T2 t2 => t2Projection(t2),
+            _ => throw new NotSupportedException()
+        };
+    }
 
     public int OrdinalIndex { get; protected set; }
 
-    public virtual bool Try<T>([MaybeNullWhen(false)] out T? value)
+    public virtual bool TryGet<T>([MaybeNullWhen(false)] out T? value)
     {
         if (0 == OrdinalIndex)
         {
@@ -42,7 +80,7 @@ public class OneOf<T1, T2>
             return false;
         }
 
-        Opt<T> setValue<TItem>(TItem item)
+        static Opt<T> setValue<TItem>(TItem item)
         {
             return item is T t ? Opt.Some(t) : Opt.None<T>();
         }
@@ -57,10 +95,16 @@ public class OneOf<T1, T2>
         value = foundValue.IsSome ? foundValue.OrThrow() : default;
         return foundValue.IsSome;
     }
+
+    public Type? SelectedType { get; protected set; }
+
+    public override string ToString() => SelectedType!.ToString();
+
+    public object? Value { get; protected set; }
 }
 
 /// <summary>
-/// Types are mutually exclusive. Just one of the types is set.
+/// This class represent disjunct alternatives of types. Types are mutually exclusive. Just one of the types is set.
 /// </summary>
 /// <typeparam name="T1"></typeparam>
 /// <typeparam name="T2"></typeparam>
@@ -85,18 +129,33 @@ public class OneOf<T1, T2, T3> : OneOf<T1, T2>
         Item3 = Opt.Some(t3);
 
         OrdinalIndex = 3;
+        SelectedType = typeof(T3);
+        Value = t3!;
+    }
+
+    public void Invoke(Action<T3> action)
+    {
+        if (Value is T3 value) action(value);
     }
 
     public Opt<T3> Item3 { get; }
 
-    public override bool Try<T>([MaybeNullWhen(false)] out T value)
+    public TResult Match<TResult>(
+        Func<T1, TResult> t1Projection,
+        Func<T2, TResult> t2Projection,
+        Func<T3, TResult> t3Projection)
     {
-        if (0 == OrdinalIndex)
+        return Value switch
         {
-            value = default;
-            return false;
-        }
+            T1 t1 => t1Projection(t1),
+            T2 t2 => t2Projection(t2),
+            T3 t3 => t3Projection(t3),
+            _ => throw new NotSupportedException()
+        };
+    }
 
+    public override bool TryGet<T>([MaybeNullWhen(false)] out T value)
+    {
         if (3 == OrdinalIndex)
         {
             if (Item3.OrThrow() is T t)
@@ -107,12 +166,12 @@ public class OneOf<T1, T2, T3> : OneOf<T1, T2>
             value = default;
             return false;
         }
-        return base.Try(out value);
+        return base.TryGet(out value);
     }
 }
 
 /// <summary>
-/// Types are mutually exclusive. Just one of the types is set.
+/// This class represent disjunct alternatives of types. Types are mutually exclusive. Just one of the types is set.
 /// </summary>
 /// <typeparam name="T1"></typeparam>
 /// <typeparam name="T2"></typeparam>
@@ -142,18 +201,35 @@ public class OneOf<T1, T2, T3, T4> : OneOf<T1, T2, T3>
         Item4 = Opt.Some(t4);
 
         OrdinalIndex = 4;
+        SelectedType = typeof(T4);
+        Value = t4!;
+    }
+
+    public void Invoke(Action<T4> action)
+    {
+        if (Value is T4 value) action(value);
     }
 
     public Opt<T4> Item4 { get; }
 
-    public override bool Try<T>([MaybeNullWhen(false)] out T value)
+    public TResult Match<TResult>(
+        Func<T1, TResult> t1Projection,
+        Func<T2, TResult> t2Projection,
+        Func<T3, TResult> t3Projection,
+        Func<T4, TResult> t4Projection)
     {
-        if (0 == OrdinalIndex)
+        return Value switch
         {
-            value = default;
-            return false;
-        }
+            T1 t1 => t1Projection(t1),
+            T2 t2 => t2Projection(t2),
+            T3 t3 => t3Projection(t3),
+            T4 t4 => t4Projection(t4),
+            _ => throw new NotSupportedException()
+        };
+    }
 
+    public override bool TryGet<T>([MaybeNullWhen(false)] out T value)
+    {
         if (4 == OrdinalIndex)
         {
             if (Item4.OrThrow() is T t)
@@ -164,12 +240,12 @@ public class OneOf<T1, T2, T3, T4> : OneOf<T1, T2, T3>
             value = default;
             return false;
         }
-        return base.Try(out value);
+        return base.TryGet(out value);
     }
 }
 
 /// <summary>
-/// Types are mutually exclusive. Just one of the types is set.
+/// This class represent disjunct alternatives of types. Types are mutually exclusive. Just one of the types is set.
 /// </summary>
 /// <typeparam name="T1"></typeparam>
 /// <typeparam name="T2"></typeparam>
@@ -184,18 +260,37 @@ public class OneOf<T1, T2, T3, T4, T5> : OneOf<T1, T2, T3, T4>
         Item5 = Opt.Some(t5);
 
         OrdinalIndex = 5;
+        SelectedType = typeof(T5);
+        Value = t5!;
+    }
+
+    public void Invoke(Action<T5> action)
+    {
+        if (Value is T5 value) action(value);
     }
 
     public Opt<T5> Item5 { get; }
 
-    public override bool Try<T>([MaybeNullWhen(false)] out T value)
+    public TResult Match<TResult>(
+        Func<T1, TResult> t1Projection,
+        Func<T2, TResult> t2Projection,
+        Func<T3, TResult> t3Projection,
+        Func<T4, TResult> t4Projection,
+        Func<T5, TResult> t5Projection)
     {
-        if (0 == OrdinalIndex)
+        return Value switch
         {
-            value = default;
-            return false;
-        }
+            T1 t1 => t1Projection(t1),
+            T2 t2 => t2Projection(t2),
+            T3 t3 => t3Projection(t3),
+            T4 t4 => t4Projection(t4),
+            T5 t5 => t5Projection(t5),
+            _ => throw new NotSupportedException()
+        };
+    }
 
+    public override bool TryGet<T>([MaybeNullWhen(false)] out T value)
+    {
         if (5 == OrdinalIndex)
         {
             if (Item5.OrThrow() is T t)
@@ -206,6 +301,6 @@ public class OneOf<T1, T2, T3, T4, T5> : OneOf<T1, T2, T3, T4>
             value = default;
             return false;
         }
-        return base.Try(out value);
+        return base.TryGet(out value);
     }
 }
