4d1f6043822c03a5d486dd7f5ec1789cc163e1b4
Markus Raufer <markus.raufer@gmail.com>
2022-06-27 22:01:03 +0200

    add IIdFactory

---
+++ Foundation6/GuidNamedIdFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace Foundation;
+
+public class GuidNamedIdFactory : IIdFactory<NamedId>
+{
+    private readonly string _name;
+    public GuidNamedIdFactory(string name)
+    {
+        _name = name.ThrowIfNullOrEmpty();
+    }
+
+    public NamedId CreateId()
+    {
+        return NamedId.New(_name, Guid.NewGuid());
+    }
+}
+
---
+++ Foundation6/IdFactory.cs
@@ -0,0 +1,36 @@
+﻿namespace Foundation;
+
+public class IdFactory : IIdFactory<Id>, IResetable
+{
+    private Id _current;
+    private readonly Func<Id, Id> _factory;
+    private readonly Id _seed;
+    private bool _passed;
+
+    public IdFactory(Id seed, Func<Id, Id> factory)
+    {
+        _seed = seed;
+        _factory = factory.ThrowIfNull();
+
+        _current = seed;
+        _passed = false;
+    }
+
+    public Id CreateId()
+    {
+        if(!_passed)
+        {
+            _passed = true;
+            return _current;
+        }
+
+        return _factory(_current);
+    }
+
+    public void Reset()
+    {
+        _current = _seed;
+        _passed = false;
+    }
+}
+
