ac0cb7647252a597957a5bd95f8a90d3c2159f6a
Markus Raufer <markus.raufer@gmail.com>
2022-06-30 12:06:23 +0200

    move KeyValueExtensions to Foundation.Collections.Generic

--- Foundation6/Collections/Generic/KeyValuePairExtensions.cs
+++ Foundation6/Collections/Generic/KeyValuePairExtensions.cs
@@ -4,6 +4,21 @@ namespace Foundation.Collections.Generic
 {
     public static class KeyValuePairExtensions
     {
+        /// <summary>
+        /// Returns Some(<typeparamref name="TValue"/>) if key found otherwise None.
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="keyValues"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static Opt<TValue> Nth<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> keyValues, TKey key)
+            where TKey : notnull
+            => keyValues.FirstAsOpt(kv => kv.Key.Equals(key))
+                        .Match(kv => Opt.Some(kv.Value),
+                               () => Opt.None<TValue>());
+
+
         public static KeyValuePair<TKey, TValue> ThrowIfEmpty<TKey, TValue>(
             this KeyValuePair<TKey, TValue> pair,
             [CallerArgumentExpression("pair")] string name = "")
@@ -14,7 +29,8 @@ namespace Foundation.Collections.Generic
         public static IEnumerable<KeyValue<TKey, TValue>> ToKeyValues<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> keyValues)
             where TKey : notnull
         {
-            return keyValues.Select(kvp => new KeyValue<TKey, TValue>(kvp.Key, kvp.Value));
+            return keyValues.ThrowIfNull()
+                            .Select(kvp => new KeyValue<TKey, TValue>(kvp.Key, kvp.Value));
         }
     }
 }
