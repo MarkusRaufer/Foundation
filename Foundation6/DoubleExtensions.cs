5f15c6875b8740b8f2650f464d92b2c66ffadc9f
Markus Raufer <markus.raufer@gmail.com>
2022-07-03 12:53:27 +0200

    refactor DoubleExtensions

--- Foundation6/DoubleExtensions.cs
+++ Foundation6/DoubleExtensions.cs
@@ -2,33 +2,33 @@
 
 public static class DoubleExtensions
 {
-    public static bool IsZero(this double value, double epsilon = double.Epsilon)
+    public static bool IsZero(this double value, double deviation = double.Epsilon)
     {
-        return Math.Abs(value) < double.Epsilon;
+        return Math.Abs(value) < deviation;
     }
 
-    public static bool Equal(this double left, double right, double epsilon = double.Epsilon)
+    public static bool Equal(this double left, double right, double deviation = double.Epsilon)
     {
-        return (Math.Abs(left) - Math.Abs(right)) < epsilon;
+        return (Math.Abs(left) - Math.Abs(right)) < deviation;
     }
 
-    public static bool GreaterThan(this double left, double right, double epsilon = double.Epsilon)
+    public static bool GreaterThan(this double left, double right, double deviation = double.Epsilon)
     {
-        return (left - right) > epsilon;
+        return (left - right) > deviation;
     }
 
-    public static bool GreaterThanOrEqual(this double left, double right, double epsilon = double.Epsilon)
+    public static bool GreaterThanOrEqual(this double left, double right, double deviation = double.Epsilon)
     {
-        return Equal(left, right, epsilon) || GreaterThan(left, right, epsilon);
+        return Equal(left, right, deviation) || GreaterThan(left, right, deviation);
     }
 
-    public static bool LessThan(this double left, double right, double epsilon = double.Epsilon)
+    public static bool LessThan(this double left, double right, double deviation = double.Epsilon)
     {
-        return (left - right) < -epsilon;
+        return (left - right) < -deviation;
     }
 
-    public static bool LessThanOrEqual(this double left, double right, double epsilon = double.Epsilon)
+    public static bool LessThanOrEqual(this double left, double right, double deviation = double.Epsilon)
     {
-        return Equal(left, right, epsilon) || LessThan(left, right, epsilon);
+        return Equal(left, right, deviation) || LessThan(left, right, deviation);
     }
 }
