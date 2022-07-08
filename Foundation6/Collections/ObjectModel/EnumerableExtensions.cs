c784ec95285cd888044e4470bc8e846fc4a2557c
Markus Raufer <markus.raufer@gmail.com>
2022-07-08 13:49:45 +0200

    refactor ObjectModel.EnumerableExtensions

--- Foundation6/Collections/ObjectModel/EnumerableExtensions.cs
+++ Foundation6/Collections/ObjectModel/EnumerableExtensions.cs
@@ -1,33 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿namespace Foundation.Collections.ObjectModel;
 
-namespace Foundation.Collections.ObjectModel
+public static class EnumerableExtensions
 {
-    public static class EnumerableExtensions
+    public static IPropertyCollection ToPropertyCollection(
+        this IEnumerable<Property> properties)
     {
-        public static IPropertyCollection ToPropertyCollection(
-            this IEnumerable<Property> properties)
-        {
-            return new PropertyCollection(properties);
-        }
+        return new PropertyCollection(properties);
+    }
 
-        public static IPropertyCollection ToPropertyCollection(this IEnumerable<KeyValuePair<string, object>> properties)
+    public static IPropertyCollection ToPropertyCollection(this IEnumerable<KeyValuePair<string, object>> properties)
+    {
+        return new PropertyCollection(properties.Select(kvp => new Property(kvp.Key, kvp.Value)));
+    }
+    
+    public static IPropertyCollection ToPropertyCollection<T, TKey, TValue>(
+        this IEnumerable<T> items, Func<T, string> keySelector, Func<T, TValue> valueSelector)
+    {
+        var properties = new PropertyCollection();
+        foreach (var item in items)
         {
-            return new PropertyCollection(properties.Select(kvp => new Property(kvp.Key, kvp.Value)));
+            var property = new Property(keySelector(item), valueSelector(item));
+            properties.Add(property);
         }
-        
-        public static IPropertyCollection ToPropertyCollection<T, TKey, TValue>(
-            this IEnumerable<T> items, Func<T, string> keySelector, Func<T, TValue> valueSelector)
-        {
-            var properties = new PropertyCollection();
-            foreach (var item in items)
-            {
-                var property = new Property(keySelector(item), valueSelector(item));
-                properties.Add(property);
-            }
-                
-            return properties;
-        }
+            
+        return properties;
     }
 }
