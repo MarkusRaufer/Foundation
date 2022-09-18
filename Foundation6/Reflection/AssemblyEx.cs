468e6d9f1c1e05b08899c9d359aa2886589e28ba
Markus Raufer <markus.raufer@gmail.com>
2022-09-18 19:51:51 +0200

    add AssemblyEx

---
+++ Foundation6/Reflection/AssemblyEx.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace Foundation.Reflection;
+
+public static class AssemblyEx
+{
+    /// <summary>
+    /// Returns the directory with the executing assembly.
+    /// </summary>
+    /// <returns></returns>
+    public static string? GetExecutionDirectory()
+    {
+        return GetExecutionDirectory(Assembly.GetExecutingAssembly());
+    }
+
+    /// <summary>
+    /// Returns the directory of the assembly.
+    /// </summary>
+    /// <param name="assembly"></param>
+    /// <returns></returns>
+    public static string? GetExecutionDirectory(Assembly assembly)
+    {
+        assembly.ThrowIfNull();
+        return Path.GetDirectoryName(assembly.Location);
+    }
+}
