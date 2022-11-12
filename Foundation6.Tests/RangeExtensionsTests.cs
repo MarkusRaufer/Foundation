5b55a450b6e3b7a9d02ceca1b47906ebed63bb1b
Markus Raufer <markus.raufer@gmail.com>
2022-11-12 21:24:59 +0100

    add RangeExtensions

---
+++ Foundation6.Tests/RangeExtensionsTests.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Foundation;
+
+[TestFixture]
+public class RangeExtensionsTests
+{
+    [Test]
+    public void GetEnumerator_Should_ReturnEnumerator_When_UsedWithForeachLoop()
+    {
+        var numbers = new List<int>();
+
+        foreach(var i in 0..10)
+        {
+            numbers.Add(i);
+        }
+
+        Assert.AreEqual(11, numbers.Count);
+        foreach(var i in Enumerable.Range(0, 11))
+        {
+            Assert.AreEqual(i, numbers[i]);
+        }
+    }
+
+    [Test]
+    public void Includes_Should_ReturnFalse_When_ValueIsOutOfRange()
+    {
+        var range = 0..10;
+
+        Assert.IsFalse(range.Includes(-1));
+        Assert.IsFalse(range.Includes(11));
+    }
+
+    [Test]
+    public void Includes_Should_ReturnTrue_When_ValueIsInRange()
+    {
+        var range = 0..10;
+        foreach (var i in Enumerable.Range(0, 11))
+        {
+            Assert.IsTrue(range.Includes(i));
+        }
+    }
+
+    [Test]
+    public void Includes_Should_ReturnTrue_When_ValueIsInRangeAndUsesEndIsFromEnd()
+    {
+        {
+            var range = 0..;
+            foreach (var i in Enumerable.Range(0, 11))
+            {
+                Assert.IsTrue(range.Includes(i));
+            }
+        }
+        {
+            var range = 0..^3;
+            foreach (var i in Enumerable.Range(0, 11))
+            {
+                Assert.IsTrue(range.Includes(i));
+            }
+        }
+    }
+
+    [Test]
+    public void Includes_Should_ReturnTrue_When_ValueIsInRangeAndUsesStartIsFromEnd()
+    {
+        {
+            var range = ..10;
+            foreach (var i in Enumerable.Range(0, 11))
+            {
+                Assert.IsTrue(range.Includes(i));
+            }
+        }
+        {
+            var range = ^3..10;
+            foreach (var i in Enumerable.Range(0, 11))
+            {
+                Assert.IsTrue(range.Includes(i));
+            }
+        }
+    }
+}
--- Foundation6/RangeExtensions.cs
+++ Foundation6/RangeExtensions.cs
@@ -1,21 +1,43 @@
-﻿namespace Foundation
+﻿namespace Foundation;
+
+public static class RangeExtensions
 {
-    public static class RangeExtensions
+    public static RangeIntEnumerator GetEnumerator(this System.Range range)
     {
-        /// <summary>
-        /// Checks if value is within Start and End. Index from end are not supported.
-        /// </summary>
-        /// <param name="range"></param>
-        /// <param name="value"></param>
-        /// <returns></returns>
-        public static bool IsInRange(this System.Range range, int value)
+        return new RangeIntEnumerator(range);
+    }
+
+    public static bool Includes(this System.Range range, int value)
+    {
+        if(!range.Start.IsFromEnd)
         {
             if (range.Start.Value > value) return false;
-
-            if (range.End.IsFromEnd) 
-                return 0 == range.End.Value;
-
+        }
+        if(!range.End.IsFromEnd)
+        {
             return range.End.Value >= value;
         }
+
+        return true;
+    }
+}
+
+public class RangeIntEnumerator
+{
+    private int _current;
+    private readonly int _end;
+
+    public RangeIntEnumerator(System.Range range)
+    {
+        _current = range.Start.IsFromEnd ? -1 : range.Start.Value - 1;
+        _end = range.End.IsFromEnd ? int.MaxValue : range.End.Value;
+    }
+
+    public int Current => _current;
+
+    public bool MoveNext()
+    {
+        _current++;
+        return _current <= _end;
     }
 }
