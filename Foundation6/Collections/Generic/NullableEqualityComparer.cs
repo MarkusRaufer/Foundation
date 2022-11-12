9dddc6b3db7662e8bb60cfa8d49aced9330b326c
Markus Raufer <markus.raufer@gmail.com>
2022-11-12 21:22:26 +0100

    refactor EqualityComparers

--- Foundation6/Collections/Generic/NullableEqualityComparer.cs
+++ Foundation6/Collections/Generic/NullableEqualityComparer.cs
@@ -2,18 +2,23 @@
 
 namespace Foundation.Collections.Generic;
 
-public class LambdaEqualityComparer<T> : EqualityComparer<T>
+public class NullableEqualityComparer<T> : EqualityComparer<T>
 {
-    private readonly Func<T?, int> _hashCodeFunc;
-    private readonly Func<T?, T?, bool> _predicate;
+    private readonly Func<T, int> _hashCodeFunc;
+    private readonly Func<T, T, bool> _predicate;
 
     public override bool Equals(T? x, T? y)
     {
+        if (null == x) return null == y;
+        if(null == y) return false;
+
         return _predicate(x, y);
     }
 
     public override int GetHashCode([DisallowNull] T obj)
     {
+        if(null == obj) return 0;
+
         return _hashCodeFunc(obj);
     }
 
@@ -21,7 +26,7 @@ public class LambdaEqualityComparer<T> : EqualityComparer<T>
     /// Default hashCodeFunc is DefaultHashCodeFunc.
     /// </summary>
     /// <param name="predicate"></param>
-    public LambdaEqualityComparer(Func<T?, T?, bool> predicate) : this(predicate, DefaultHashCodeFunc)
+    public NullableEqualityComparer(Func<T, T, bool> predicate) : this(predicate, DefaultHashCodeFunc)
     {
     }
 
@@ -29,19 +34,19 @@ public class LambdaEqualityComparer<T> : EqualityComparer<T>
     /// Default predicate uses Equals method.
     /// </summary>
     /// <param name="hashCodeFunc"></param>
-    public LambdaEqualityComparer(Func<T?, int>? hashCodeFunc) : this(DefaultPredicate, hashCodeFunc)
+    public NullableEqualityComparer(Func<T, int> hashCodeFunc) : this(DefaultPredicate, hashCodeFunc)
     {
     }
 
-    public LambdaEqualityComparer(Func<T?, T?, bool> predicate, Func<T?, int>? hashCodeFunc)
+    public NullableEqualityComparer(Func<T, T, bool> predicate, Func<T, int>? hashCodeFunc)
     {
         _predicate = predicate.ThrowIfNull();
         _hashCodeFunc = hashCodeFunc.ThrowIfNull();
     }
 
-    public static Func<T?, int> DefaultHashCodeFunc { get; } = (t => null == t ? 0 : t.GetHashCode());
+    public static Func<T, int> DefaultHashCodeFunc { get; } = (t => null == t ? 0 : t.GetHashCode());
 
-    public static Func<T?, T?, bool> DefaultPredicate { get; } =
+    public static Func<T, T, bool> DefaultPredicate { get; } =
         (x, y) =>
         {
             if (null == x) return null == y;
