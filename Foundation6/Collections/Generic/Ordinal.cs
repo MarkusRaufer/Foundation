af884998e5039effff7b2487088cace154b53028
Markus Raufer <markus.raufer@gmail.com>
2023-02-06 18:05:24 +0100

    line breaks

--- Foundation6/Collections/Generic/Ordinal.cs
+++ Foundation6/Collections/Generic/Ordinal.cs
@@ -4,7 +4,7 @@
     /// Represents a value with an ordinal position of a list.
     /// </summary>
     /// <typeparam name="T">The type of the value.</typeparam>
-    /// <param name="Position">The ordinal position of a list.</param>
+    /// <param name="Position">The ordinal position in a list.</param>
     /// <param name="Value">The positioned value.</param>
     public record struct Ordinal<T>(int Position, T Value) : IComparable<Ordinal<T>>
     {
