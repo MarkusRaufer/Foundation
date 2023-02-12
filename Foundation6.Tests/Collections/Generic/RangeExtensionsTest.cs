bbbc0bcfaeb593bde56f5ce0989135f86d7d0bcc
Markus Raufer <markus.raufer@gmail.com>
2023-02-12 17:34:33 +0100

    feat(RangeExtensions):

---
+++ Foundation6.Tests/Collections/Generic/RangeExtensionsTest.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foundation.Collections.Generic;
+
+[TestFixture]
+public class RangeExtensionsTest
+{
+    [Test]
+    public void Enumerator_Should_ThrowException_When_RangeEndIsUndefined()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            var i = 0;
+            foreach (var number in 0..)
+            {
+                Assert.AreEqual(i, number);
+                i++;
+            }
+
+        });
+    }
+
+    [Test]
+    public void Enumerator_Should_Return_5Integers_When_RangeStart0AndRangeEnd5()
+    {
+        var i = 0;
+        foreach(var number in 0..5)
+        {
+            i.Should().Be(number);
+            i++;
+        }
+    }
+
+    [Test]
+    public void Enumerator_Should_Return_5Integers_When_RangeStart5AndRangeEnd10()
+    {
+        var i = 5;
+        foreach (var number in 5..10)
+        {
+            i.Should().Be(number);
+            i++;
+        }
+    }
+}
---
+++ Foundation6/Collections/Generic/RangeExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace Foundation.Collections.Generic;
+
+public static class RangeExtensions
+{
+	public static RangeEnumerator GetEnumerator(this System.Range range) => new(range);
+}
+
+public ref struct RangeEnumerator
+{
+	private readonly System.Range _range;
+	private int _current;
+
+	public RangeEnumerator(System.Range range)
+	{
+		_range = range.ThrowIfOutOfRange(() => range.End.IsFromEnd, "range end must be defined");
+		_current = range.Start.IsFromEnd ? -1 : range.Start.Value - 1;
+    }
+
+	public int Current => _current;
+
+	public bool MoveNext()
+	{
+        _current++;
+
+        if (_range.End.IsFromEnd) return true;
+
+		if (_current > _range.End.Value) return false;
+		
+		return true;
+	}
+}
