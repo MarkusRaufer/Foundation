51c7e3104770f06b04ac8eb7d258a2baec7e1e2a
Markus Raufer <markus.raufer@gmail.com>
2023-02-15 14:41:49 +0100

    feat(Foundation.Algorithms): add Foundation.Algorithms

---
+++ Foundation.Algorithms/BloomFilter.cs
@@ -0,0 +1,61 @@
+﻿using System.Security.Cryptography;
+
+namespace Foundation.Algorithms;
+
+public abstract class BloomFilter<T>
+{
+    private readonly int[] _bits;
+    private readonly int _hashFunctions;
+    private readonly HashAlgorithm _hashAlgorithm;
+
+    public BloomFilter(int size, int hashFunctions, HashAlgorithm hashAlgorithm)
+    {
+        _bits = new int[size];
+        _hashFunctions = hashFunctions;
+        _hashAlgorithm = hashAlgorithm;
+    }
+
+    public void Add(T item)
+    {
+        var data = GetHash(item);
+        for (int i = 0; i < _hashFunctions; i++)
+        {
+            int index = GetIndex(data, i, _bits.Length);
+            _bits[index] = 1;
+        }
+    }
+
+    public bool Contains(T item)
+    {
+        var data = GetHash(item);
+        for (int i = 0; i < _hashFunctions; i++)
+        {
+            int index = GetIndex(data, i, _bits.Length);
+            if (_bits[index] == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public abstract byte[] GetBytes(T item);
+
+    private byte[] GetHash(T item)
+    {
+        ArgumentNullException.ThrowIfNull(item);
+
+        var bytes = GetBytes(item);
+        return _hashAlgorithm.ComputeHash(bytes);
+    }
+
+    private int GetIndex(byte[] data, int hashFunctionIndex, int size)
+    {
+        int hash = 0;
+        for (int i = hashFunctionIndex; i < data.Length; i += _hashFunctions)
+        {
+            hash = (hash * 31) + data[i];
+        }
+        return Math.Abs(hash % size);
+    }
+}
---
+++ Foundation.Algorithms/GenericBloomFilter.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Foundation.Algorithms;
+
+public class GenericBloomFilter<T> : BloomFilter<T>
+{
+    public GenericBloomFilter(int size, int hashFunctions, HashAlgorithm hashAlgorithm)
+        : base(size, hashFunctions, hashAlgorithm)
+    {
+    }
+
+    public override byte[] GetBytes(T item)
+    {
+        ArgumentNullException.ThrowIfNull(item);
+
+        return Encoding.UTF8.GetBytes(item.ToString()!);
+    }
+}
