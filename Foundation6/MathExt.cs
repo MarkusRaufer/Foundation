3cea567a7ef57fd7cde75f085b0122f05638d956
Markus Raufer <markus.raufer@gmail.com>
2023-02-19 15:48:14 +0100

    refactor(Foundation)

---
+++ Foundation6/MathExt.cs
@@ -0,0 +1,12 @@
+﻿namespace Foundation;
+
+public static class MathExt
+{
+    public static (T min, T max) MinMax<T>(T lhs, T rhs)
+        where T : IComparable<T>
+    {
+        var cmp = lhs.CompareNullableTo(rhs);
+
+        return 1 > cmp ? (lhs, rhs) : (rhs, lhs);
+    }
+}
