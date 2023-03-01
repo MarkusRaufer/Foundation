bb2523d2f893267635959117a193115383076e02
Markus Raufer <markus.raufer@gmail.com>
2023-03-01 18:28:45 +0100

    refactor: GenericEntity

--- Foundation6/ComponentModel/GenericEntity.cs
+++ Foundation6/ComponentModel/GenericEntity.cs
@@ -1,17 +1,9 @@
 ﻿namespace Foundation.ComponentModel;
 
-public abstract class GenericEntity<TId> : IIdentifiable<NamedId<TId>>
+public abstract class GenericEntity<TId> : Entity<NamedId<TId>>
     where TId : struct, IComparable<TId>, IEquatable<TId>
 {
-    protected GenericEntity(NamedId<TId> id) => Id = id;
-
-    public override bool Equals(object? obj) => Equals(obj as IIdentifiable<NamedId<TId>>);
-
-    public bool Equals(IIdentifiable<NamedId<TId>>? other) => null != other && Id.Equals(other.Id);
-
-    public override int GetHashCode() => Id.GetHashCode();
-
-    public NamedId<TId> Id { get; }
-
-    public override string ToString() => $"Id: {Id}";
+    protected GenericEntity(NamedId<TId> id) : base(id)
+    {
+    }
 }
