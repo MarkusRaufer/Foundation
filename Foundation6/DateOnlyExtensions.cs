427da8aa56c0c45bc87f73c58a15e6f662873671
Markus Raufer <markus.raufer@gmail.com>
2023-05-17 18:23:58 +0200

    refactor DateOnlyExtensions

--- Foundation6/DateOnlyExtensions.cs
+++ Foundation6/DateOnlyExtensions.cs
@@ -1,30 +1,38 @@
-﻿namespace Foundation
+﻿namespace Foundation;
+
+public static class DateOnlyExtensions
 {
-    public static class DateOnlyExtensions
+    public static DateOnly Add(this DateOnly date, TimeSpan span)
     {
-        public static DateOnly Add(this DateOnly date, TimeSpan span)
-        {
-            return DateOnly.FromDateTime(date.ToDateTime().Add(span));
-        }
-
-        public static TimeSpan Subtract(this DateOnly date, DateOnly subtract)
-            => TimeSpan.FromDays(date.DayNumber - subtract.DayNumber);
-
-        public static DateOnly Subtract(this DateOnly date, TimeSpan span)
-            => DateOnly.FromDateTime(date.ToDateTime().Subtract(span));
-
-        public static DateTime ToDateTime(this DateOnly date)
-        {
-            return new DateTime(date.Year, date.Month, date.Day);
-        }
-
-        public static DateTime ToDateTime(this DateOnly date, DateTimeKind kind)
-        {
-            return new DateTime(date.Year, date.Month, date.Day, 0, 0, 0, kind);
-        }
-
-        public static long ToTicks(this DateOnly date) => date.ToDateTime().Ticks;
-
-        public static long ToTicks(this DateOnly date, DateTimeKind kind) => date.ToDateTime(kind).Ticks;
+        return DateOnly.FromDateTime(date.ToDateTime().Add(span));
     }
+
+    public static TimeSpan Subtract(this DateOnly date, DateOnly subtract)
+        => TimeSpan.FromDays(date.DayNumber - subtract.DayNumber);
+
+    public static DateOnly Subtract(this DateOnly date, TimeSpan span)
+        => DateOnly.FromDateTime(date.ToDateTime().Subtract(span));
+
+    public static DateOnly SubtractDays(this DateOnly date, int days)
+        => date.AddDays(days * -1);
+
+    public static DateOnly SubtractMonths(this DateOnly date, int months)
+        => date.AddMonths(months * -1);
+
+    public static DateOnly SubtractYears(this DateOnly date, int years)
+        => date.AddMonths(years * -1);
+
+    public static DateTime ToDateTime(this DateOnly date)
+    {
+        return new DateTime(date.Year, date.Month, date.Day);
+    }
+
+    public static DateTime ToDateTime(this DateOnly date, DateTimeKind kind)
+    {
+        return new DateTime(date.Year, date.Month, date.Day, 0, 0, 0, kind);
+    }
+
+    public static long ToTicks(this DateOnly date) => date.ToDateTime().Ticks;
+
+    public static long ToTicks(this DateOnly date, DateTimeKind kind) => date.ToDateTime(kind).Ticks;
 }
