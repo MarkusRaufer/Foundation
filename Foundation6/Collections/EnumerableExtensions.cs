de0a53fcfb9ab4729242f206c5f4faf1062282db
Markus Raufer <markus.raufer@gmail.com>
2023-08-21 16:58:28 +0200

    refactor(EnumerableExtensions): remove misplaced methods

--- Foundation6/Collections/EnumerableExtensions.cs
+++ Foundation6/Collections/EnumerableExtensions.cs
@@ -307,37 +307,6 @@ public static class EnumerableExtensions
         }
     }
 
-    /// <summary>
-    /// Like <see cref="=Select<typeparamref name="T"/>"/>
-    /// </summary>
-    /// <param name="items"></param>
-    /// <param name="selector"></param>
-    /// <returns></returns>
-    public static IEnumerable SelectObject(this IEnumerable items, Func<object, object> selector)
-    {
-        items.ThrowIfNull();
-        selector.ThrowIfNull();
-
-        foreach (var item in items)
-            yield return selector(item);
-    }
-
-    /// <summary>
-    /// Like <see cref="=Select<typeparamref name="T"/>"/>
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="items"></param>
-    /// <param name="selector"></param>
-    /// <returns></returns>
-    public static IEnumerable<T> SelectObject<T>(this IEnumerable items, Func<object, T> selector)
-    {
-        items.ThrowIfNull();
-        selector.ThrowIfNull();
-
-        foreach (var item in items)
-            yield return selector(item);
-    }
-
     public static IEnumerable SelectObjectByIndex(this IEnumerable items, Func<long, bool> selector)
     {
         items.ThrowIfNull();
@@ -364,42 +333,6 @@ public static class EnumerableExtensions
         if (enumerator.MoveNext()) throw new InvalidOperationException("more than one element");
 
         return current;
-    }
-
-    /// <summary>
-    /// Convert to typed enumerable.
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="items"></param>
-    /// <returns></returns>
-    public static IEnumerable<T> ToEnumerable<T>(this IEnumerable items) => items.CastTo<T>();
-
-    public static IList<T> ToList<T>(this IEnumerable items)
-    {
-        var list = new List<T>();
-        foreach (var item in items)
-            list.Add((T)item);
-
-        return list;
-    }
-
-    public static object?[] ToObjectArray(this IEnumerable items)
-    {
-        var list = new ArrayList();
-        items.ForEachObject(i => list.Add(i));
-        return list.ToArray();
-    }
-
-    public static IList ToObjectList(this IEnumerable items)
-    {
-        var list = new ArrayList();
-        items.ForEachObject(i => list.Add(i));
-        return list;
-    }
-
-    public static IReadOnlyCollection<T> ToReadOnlyCollection<T>(this IEnumerable<T> items)
-    {
-        return ReadOnlyCollection.New(items);
     }
 }
 
