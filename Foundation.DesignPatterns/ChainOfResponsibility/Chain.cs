17dd080f00b73985677b65e2ea611ff00ec5fc00
Markus Raufer <markus.raufer@gmail.com>
2023-08-21 16:52:05 +0200

    add design pattern: chain of responsibility

---
+++ Foundation.DesignPatterns/ChainOfResponsibility/Chain.cs
@@ -0,0 +1,35 @@
+﻿using Foundation.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foundation.DesignPatterns.ChainOfResponsibility;
+
+public class Chain
+{
+    public static IEnumerable<ChainHandler<TIn, TOut>> Handlers<TIn, TOut>(IEnumerable<Func<TIn, Option<TOut>>> handlers)
+    {
+        var prevHandler = handlers.FirstOrDefault();
+        if (prevHandler is null) yield break;
+
+        var prevChainHandler = ChainHandler.New(prevHandler);
+        yield return prevChainHandler;
+
+        foreach (var handler in handlers.Skip(1))
+        {
+            prevChainHandler = ChainHandler.New(handler, prevChainHandler);
+            yield return prevChainHandler;
+        }
+    }
+
+    public static IEnumerable<ChainHandler<TIn, TOut>> Handlers<TIn, TOut>(IEnumerable<ChainHandler<TIn, TOut>> handlers)
+    {
+        return handlers.Pairs((lhs, rhs) =>
+        {
+            return (lhs, new ChainHandler<TIn, TOut>(rhs.TryHandle, lhs));
+        });
+    }
+}
---
+++ Foundation.DesignPatterns/ChainOfResponsibility/ChainHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Foundation.DesignPatterns.ChainOfResponsibility;
+
+public static class ChainHandler
+{
+    public static ChainHandler<TIn, TOut> New<TIn, TOut>(Func<TIn, Option<TOut>> TryHandle)
+        => new(TryHandle, null);
+
+    public static ChainHandler<TIn, TOut> New<TIn, TOut>(Func<TIn, Option<TOut>> TryHandle, ChainHandler<TIn, TOut> Successor)
+        => new(TryHandle, Successor);
+}
+
+public record ChainHandler<TIn, TOut>(Func<TIn, Option<TOut>> TryHandle, ChainHandler<TIn, TOut>? Successor)
+{
+    public Option<TOut> Handle(TIn @in)
+    {
+        var result = TryHandle(@in);
+        if(result.IsSome) return result;
+        
+        return Successor is null ? Option.None<TOut>() : Successor.Handle(@in);
+    }
+}
