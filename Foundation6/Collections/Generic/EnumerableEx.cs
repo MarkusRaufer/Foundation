d15e5cfa251c0923fb56523aec97ea720cc92d59
Markus Raufer <markus.raufer@gmail.com>
2023-08-31 18:57:23 +0200

    refactor: remove unnecessary artefacts

--- Foundation6/Collections/Generic/EnumerableEx.cs
+++ Foundation6/Collections/Generic/EnumerableEx.cs
@@ -22,63 +22,111 @@ public static class EnumerableEx
         }
     }
 
+    /// <summary>
+    /// Generates a sequence of characters between a minimum and maximum. The number of items is not needed. E.g. min == 'A' and max == 'Z'.
+    /// </summary>
+    /// <param name="min">The start value.</param>
+    /// <param name="max">The end value.</param>
+    /// <returns></returns>
     public static IEnumerable<char> Range(char min, char max)
     {
         for (var i = min; i <= max; i++)
             yield return i;
     }
 
-    public static IEnumerable<int> Range(int min, int max)
-    {
-        for (var i = min; i <= max; i++)
-            yield return i;
-    }
-
-    public static IEnumerable<long> Range(long min, long max)
-    {
-        for (var i = min; i <= max; i++)
-            yield return i;
-    }
-
-    public static IEnumerable<decimal> Range(decimal min, decimal max, decimal increment)
-    {
-        for (var i = min; i <= max; i += increment)
-            yield return i;
-    }
-
-    public static IEnumerable<double> Range(double min, double max, double increment)
-    {
-        for (var i = min; i <= max; i += increment)
-            yield return i;
-    }
-
+    /// <summary>
+    /// Generates a sequence of DateOnly values between a minimum and maximum increased by increment.
+    /// The number of items is not needed. E.g. min == 23.4 and max == 76.3
+    /// </summary>
+    /// <param name="min">The start value.</param>
+    /// <param name="max">The end value.</param>
+    /// <param name="increment">The TimeSpan of the increment.</param>
+    /// <returns></returns>
     public static IEnumerable<DateOnly> Range(DateOnly min, DateOnly max, TimeSpan increment)
     {
         DateOnly inc(DateOnly date) => date.Add(increment);
         return Range(min, max, inc);
     }
 
+    /// <summary>
+    /// Generates a sequence of DateTime values between a minimum and maximum increased by increment.
+    /// The number of items is not needed. E.g. min == 23.4 and max == 76.3
+    /// </summary>
+    /// <param name="min">The start value.</param>
+    /// <param name="max">The end value.</param>
+    /// <param name="increment">The TimeSpan of the increment.</param>
+    /// <returns></returns>
     public static IEnumerable<DateTime> Range(DateTime min, DateTime max, TimeSpan increment)
     {
         return Range(min, max, x => x + increment);
     }
 
+    /// <summary>
+    /// Generates a sequence of decimal values between a minimum and maximum increased by increment.
+    /// The number of items is not needed. E.g. min == 23.4 and max == 76.3
+    /// </summary>
+    /// <param name="min">The start value.</param>
+    /// <param name="max">The end value.</param>
+    /// <param name="increment">The size of the increment.</param>
+    /// <returns></returns>
+    public static IEnumerable<decimal> Range(decimal min, decimal max, decimal increment)
+    {
+        for (var i = min; i <= max; i += increment)
+            yield return i;
+    }
+
+    /// <summary>
+    /// Generates a sequence of double values between a minimum and maximum increased by increment.
+    /// The number of items is not needed. E.g. min == 23.4 and max == 76.3
+    /// </summary>
+    /// <param name="min">The start value.</param>
+    /// <param name="max">The end value.</param>
+    /// <param name="increment">The size of the increment.</param>
+    /// <returns></returns>
+    public static IEnumerable<double> Range(double min, double max, double increment)
+    {
+        for (var i = min; i <= max; i += increment)
+            yield return i;
+    }
+
+    /// <summary>
+    /// Generates a sequence of integers between a minimum and maximum. The number of items is not needed. E.g. min == 234 and max == 763
+    /// </summary>
+    /// <param name="min">The start value.</param>
+    /// <param name="max">The end value.</param>
+    /// <returns></returns>
+    public static IEnumerable<int> Range(int min, int max)
+    {
+        for (var i = min; i <= max; i++)
+            yield return i;
+    }
+
+    /// <summary>
+    /// Generates a sequence of long values between a minimum and maximum. The number of items is not needed. E.g. min == 234 and max == 763
+    /// </summary>
+    /// <param name="min">The start value.</param>
+    /// <param name="max">The end value.</param>
+    /// <returns></returns>
+    public static IEnumerable<long> Range(long min, long max)
+    {
+        for (var i = min; i <= max; i++)
+            yield return i;
+    }
+    
+    
+    /// <summary>
+    /// Generates a sequence of values between a minimum and maximum increased by increment.
+    /// The number of items is not needed. E.g. min == 23.4 and max == 76.3
+    /// </summary>
+    /// <param name="min">The start value.</param>
+    /// <param name="max">The end value.</param>
+    /// <param name="increment">The new incremented value.</param>
+    /// <returns></returns>
     public static IEnumerable<T> Range<T>(T min, T max, Func<T, T> increment)
         where T : notnull, IComparable<T>
     {
         var current = min;
         while (current.CompareTo(max) < 1)
-        {
-            yield return current;
-            current = increment(current);
-        }
-    }
-
-    public static IEnumerable<T> Range<T>(T min, T max, Func<T, T, int> compare, Func<T, T> increment)
-        where T : notnull
-    {
-        var current = min;
-        while (compare(current, max) < 1)
         {
             yield return current;
             current = increment(current);
