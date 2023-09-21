34226859ee101e045bc73c64f1f832294d4d1863
Markus Raufer <markus.raufer@gmail.com>
2023-09-21 17:57:20 +0200

    doc: Any

--- Foundation6/Any.cs
+++ Foundation6/Any.cs
@@ -1,3 +1,6 @@
 ﻿namespace Foundation;
 
+/// <summary>
+/// This atomic represents any type which can be used with generic types and interfaces. E.g. <see cref="IEquatable{Any}"/>
+/// </summary>
 public record struct Any;
