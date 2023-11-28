69002f02e66f972193e86b5dc664c76d4b96e249
Markus Raufer <markus.raufer@gmail.com>
2023-11-28 12:09:49 +0100

    feat(Identifiable): new decorator

---
+++ Foundation6/Identifiable.cs
@@ -0,0 +1,10 @@
+﻿namespace Foundation;
+
+/// <summary>
+/// Decorator to make an object identifiable.
+/// </summary>
+/// <typeparam name="TId">The type of the identifier.</typeparam>
+/// <typeparam name="TSubject">Type of the object.</typeparam>
+/// <param name="Id">The identifier of the object.</param>
+/// <param name="Subject">The object to be identified.</param>
+public record Identifiable<TId, TSubject>(TId Id, TSubject Subject) : IIdentifiable<TId>;
