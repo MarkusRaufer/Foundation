dde20c25d5f28ad3926e2ad08bb026d3b7ab9e7f
Markus Raufer <markus.raufer@gmail.com>
2023-12-03 20:10:14 +0100

    refactor(Foundation): move IClassifiable to Foundation.ComponentModel

---
+++ Foundation6/ComponentModel/IClassifiable.cs
@@ -0,0 +1,10 @@
+﻿namespace Foundation.ComponentModel;
+
+/// <summary>
+/// This can be used to classify an object like entity, value object.
+/// </summary>
+/// <typeparam name="TStereoType"></typeparam>
+public interface IClassifiable<TStereoType>
+{
+    TStereoType StereoType { get; }
+}
