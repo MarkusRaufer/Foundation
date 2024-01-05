6a486d84a46f733b761151b26e47ff951e8f4d38
Markus Raufer <markus.raufer@gmail.com>
2024-01-05 15:12:04 +0100

    refactor(Foundation.Text.Json): add Utf8JsonReaderExtensions, Utf8JsonWriterExtensions

---
+++ Foundation6/Text/Json/Utf8JsonReaderExtensions.cs
@@ -0,0 +1,97 @@
+﻿using System.Reflection;
+using System.Text.Json;
+
+namespace Foundation.Text.Json;
+
+public static class Utf8JsonReaderExtensions
+{
+    public static object? GetValue(this Utf8JsonReader reader, Type type)
+    {
+        switch (Type.GetTypeCode(type))
+        {
+            case TypeCode.Boolean: return reader.GetBoolean();
+            case TypeCode.Byte: return reader.GetByte();
+            case TypeCode.Char: return reader.GetString();
+            case TypeCode.DateTime: return reader.GetDateTime();
+            case TypeCode.Decimal: return reader.GetDecimal();
+            case TypeCode.Double: return reader.GetDouble();
+            case TypeCode.Int16: return reader.GetInt16();
+            case TypeCode.Int32: return reader.GetInt32();
+            case TypeCode.Int64: return reader.GetInt64();
+            case TypeCode.UInt16: return reader.GetUInt16();
+            case TypeCode.UInt32: return reader.GetUInt32();
+            case TypeCode.UInt64: return reader.GetUInt64();
+            case TypeCode.SByte: return reader.GetSByte();
+            case TypeCode.Single: return reader.GetSingle();
+            case TypeCode.String: return reader.GetString();
+        }
+
+        switch(type)
+        {
+            case Type _ when type == typeof(DateOnly):
+                {
+                    var str = reader.GetString();
+                    if(str is null) return null;
+
+                    if (DateTime.TryParse(str, out var dt)) return dt.ToDateOnly();
+                    return null;
+                }
+            case Type _ when type == typeof(Guid):
+                {
+                    var guid = reader.GetGuid();
+                    return guid;
+                }
+            case Type _ when type == typeof(TimeOnly):
+                {
+                    var str = reader.GetString();
+                    if (str is null) return null;
+
+                    if (DateTime.TryParse(str, out var dt)) return dt.ToTimeOnly();
+                    return null;
+                }
+        };
+
+        return reader.GetString();
+    }
+
+    public static object? GetValue(this Utf8JsonReader reader, string typeName)
+    {
+        switch (typeName)
+        {
+            case "System.Boolean": return reader.GetBoolean();
+            case "System.Byte": return reader.GetByte();
+            case "System.Char": return reader.GetString();
+            case "System.DateOnly":
+                {
+                    var str = reader.GetString();
+                    if (str is null) return null;
+
+                    if (DateTime.TryParse(str, out var dt)) return dt.ToDateOnly();
+                    return null;
+                }
+            case "System.DateTime": return reader.GetDateTime();
+            case "System.Decimal": return reader.GetDecimal();
+            case "System.Double": return reader.GetDouble();
+            case "System.Guid": return reader.GetGuid();
+            case "System.Int16": return reader.GetInt16();
+            case "System.Int32": return reader.GetInt32();
+            case "System.Int64": return reader.GetInt64();
+            case "System.UInt16": return reader.GetUInt16();
+            case "System.UInt32": return reader.GetUInt32();
+            case "System.UInt64": return reader.GetUInt64();
+            case "System.SByte": return reader.GetSByte();
+            case "System.Single": return reader.GetSingle();
+            case "System.String": return reader.GetString();
+            case "System.TimeOnly":
+                {
+                    var str = reader.GetString();
+                    if (str is null) return null;
+
+                    if (DateTime.TryParse(str, out var dt)) return dt.ToTimeOnly();
+                    return null;
+                }
+        }
+
+        return reader.GetString();
+    }
+}
---
+++ Foundation6/Text/Json/Utf8JsonWriterExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+
+namespace Foundation.Text.Json;
+
+public static class Utf8JsonWriterExtensions
+{
+    public static void WriteValue(this Utf8JsonWriter writer, object value)
+    {
+        if (null == value) return;
+
+        var type = value.GetType();
+        var typeCode = Type.GetTypeCode(type);
+
+        switch (typeCode)
+        {
+            case TypeCode.Boolean: writer.WriteBooleanValue((bool)value); return;
+            case TypeCode.DateTime: writer.WriteStringValue((DateTime)value); return;
+            case TypeCode.Decimal: writer.WriteNumberValue((decimal)value); return;
+            case TypeCode.Double: writer.WriteNumberValue((double)value); return;
+            case TypeCode.Int16: writer.WriteNumberValue((Int16)value); return;
+            case TypeCode.Int32: writer.WriteNumberValue((int)value); return;
+            case TypeCode.Int64: writer.WriteNumberValue((long)value); return;
+            case TypeCode.UInt16: writer.WriteNumberValue((UInt16)value); return;
+            case TypeCode.UInt32: writer.WriteNumberValue((uint)value); return;
+            case TypeCode.UInt64: writer.WriteNumberValue((ulong)value); return;
+            case TypeCode.Single: writer.WriteNumberValue((float)value); return;
+            case TypeCode.String: writer.WriteStringValue((string)value); return;
+        }
+
+        switch(value)
+        {
+            case DateOnly x: writer.WriteStringValue($"{x:yyyy-MM-dd}"); return;
+            case Guid x: writer.WriteStringValue(x); return;
+            case TimeOnly x: writer.WriteStringValue($"{x:HH:mm:ss}"); return;
+        };
+        writer.WriteStringValue($"{value}");
+    }
+}
