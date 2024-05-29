8fcf98d124bf96ac82854e3a3ec05185516431cc
Markus Raufer <markus.raufer@gmail.com>
2024-05-29 20:29:45 +0200

    feat(Foundation.ComponentModel): INotifyChanged

---
+++ Foundation6/ComponentModel/INotifyChanged.cs
@@ -0,0 +1,11 @@
+﻿namespace Foundation.ComponentModel;
+
+public interface INotifyChanged<T> : INotifyChanged<T, Action<T>>
+{
+}
+
+public interface INotifyChanged<T, TDelegate>
+    where TDelegate : Delegate
+{
+    Event<TDelegate> OnChanged { get; }
+}
