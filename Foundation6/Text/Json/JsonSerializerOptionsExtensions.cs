461901ff568a20b726435987ec337f187fab658d
Markus Raufer <markus.raufer@gmail.com>
2024-06-20 17:20:40 +0200

    feat(Foundation.Text.Json): add JsonSerializerOptionsExtensions

---
+++ Foundation6/Text/Json/JsonSerializerOptionsExtensions.cs
@@ -0,0 +1,17 @@
+﻿#if NET6_0_OR_GREATER
+
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace Foundation.Text.Json;
+
+public static class JsonSerializerOptionsExtensions
+{
+    public static JsonSerializerOptions AddConverter<TConverter>(this JsonSerializerOptions options, TConverter converter)
+        where TConverter : JsonConverter
+    {
+        options.Converters.Add(converter);
+        return options;
+    }
+}
+#endif
