dd0c36917b2266dc82d240cfc8e5888843053e33
Markus Raufer <markus.raufer@gmail.com>
2024-06-20 09:17:01 +0200

    feat(Foundation.Text.Json.Serialization): add CollectionConverter and HashSetValueJsonConverter

---
+++ Foundation6.Tests/Text/Json/Serialization/HashSetValueConverterTests.cs
@@ -0,0 +1,40 @@
+﻿#if NET6_0_OR_GREATER
+using FluentAssertions;
+using Foundation.Collections.Generic;
+using NUnit.Framework;
+using System.Text.Json;
+
+namespace Foundation.Text.Json.Serialization
+{
+    [TestFixture]
+    public class HashSetValueConverterTests
+    {
+        [Test]
+        public void DeserialzeToJson_Should_ReturnJsonString_When_Called_JsonSerializer_Serialize()
+        {
+            var sut = HashSetValue.New(1, 2, 3);
+
+            var jsonOptions = new JsonSerializerOptions
+            {
+                Converters = { new HashSetValueJsonConverter<int>() }
+            };
+
+            var json = JsonSerializer.Serialize(sut, jsonOptions);
+
+            var deserialized = JsonSerializer.Deserialize<HashSetValue<int>>(json, jsonOptions);
+
+            sut.Should().BeEquivalentTo(deserialized);
+        }
+
+        [Test]
+        public void SerialzeToJson_Should_ReturnJsonString_When_Called_JsonSerializer_Serialize()
+        {
+            var sut = HashSetValue.New(1, 2, 3);
+
+            var json = JsonSerializer.Serialize(sut);
+
+            json.Should().Be("[1,2,3]");
+        }
+    }
+}
+#endif
