7061c2f539132e729741ee99f096dd4d3e84341c
Markus Raufer <markus.raufer@gmail.com>
2024-07-03 20:06:51 +0200

    feat(Foundation): add Version.

---
+++ Foundation6.Tests/VersionTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Foundation;
+
+[TestFixture]
+public class VersionTests
+{
+    [Test]
+    public void Ctor_Should_ReturnValidVersion_When_MajorIs1_MinorIs2_PatchIs3()
+    {
+        var sut = new Version(1, 2, 3);
+
+        sut.Major.Should().Be(1);
+        sut.Minor.Should().Be(2);
+        sut.Patch.Should().Be(3);
+    }
+
+    [Test]
+    public void IncrementMajor_Should_ReturnVersionWithIncrementedMajor_When_Called()
+    {
+        var sut = new Version(1, 2, 3);
+        var version = sut.IncrementMajor();
+
+        version.Major.Should().Be(2);
+        version.Minor.Should().Be(2);
+        version.Patch.Should().Be(3);
+    }
+
+    [Test]
+    public void IncrementMinor_Should_ReturnVersionWithIncrementedMinor_When_Called()
+    {
+        var sut = new Version(1, 2, 3);
+        var version = sut.IncrementMinor();
+
+        version.Major.Should().Be(1);
+        version.Minor.Should().Be(3);
+        version.Patch.Should().Be(3);
+    }
+
+    [Test]
+    public void IncrementPatch_Should_ReturnVersionWithIncrementedPatch_When_Called()
+    {
+        var sut = new Version(1, 2, 3);
+        var version = sut.IncrementPatch();
+
+        version.Major.Should().Be(1);
+        version.Minor.Should().Be(2);
+        version.Patch.Should().Be(4);
+    }
+}
---
+++ Foundation6/Version.cs
@@ -0,0 +1,88 @@
+﻿#if NET8_0_OR_GREATER
+
+using Foundation.Buffers;
+
+#endif
+
+namespace Foundation;
+
+/// <summary>
+/// This version object corresponds to semantic versioning.
+/// </summary>
+/// <param name="Major"></param>
+/// <param name="Minor"></param>
+/// <param name="Patch"></param>
+public record struct Version(int Major, int Minor, int Patch)
+{
+    /// <summary>
+    /// Increments the major part of the version.
+    /// </summary>
+    /// <returns></returns>
+    public readonly Version IncrementMajor() => new(Major + 1, Minor, Patch);
+
+    /// <summary>
+    /// Increments the minor part of the version.
+    /// </summary>
+    /// <returns></returns>
+    public readonly Version IncrementMinor() => new(Major, Minor + 1, Patch);
+
+    /// <summary>
+    /// Increments the patch part of the version.
+    /// </summary>
+    /// <returns></returns>
+    public readonly Version IncrementPatch() => new(Major, Minor, Patch + 1);
+
+    public static Version? Parse(string version)
+    {
+        if (string.IsNullOrWhiteSpace(version)) return null;
+
+        var span = version.AsSpan();
+
+        ReadOnlySpan<char> separators = stackalloc char[1] { '.' };
+
+#if NET8_0_OR_GREATER
+        var enumerator = span.Split(separators);
+
+        //major
+        if (!enumerator.MoveNext()) return null;
+
+        if (!int.TryParse(enumerator.Current, out var major)) return null;
+
+        //minor
+        if (!enumerator.MoveNext()) return null;
+
+        if (!int.TryParse(enumerator.Current, out var minor)) return null;
+
+        //patch
+        if (!enumerator.MoveNext()) return null;
+
+        if (!int.TryParse(enumerator.Current, out var patch)) return null;
+
+        return new Version(major, minor, patch);
+#else
+        var parts = version.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+        if (parts.Length != 3) return null;
+        
+        //major
+        if (!int.TryParse(parts[0], out var major)) return null;
+
+        //minor
+        if (!int.TryParse(parts[1], out var minor)) return null;
+
+        //patch
+        if (!int.TryParse(parts[2], out var patch)) return null;
+
+        return new Version(major, minor, patch);
+#endif
+
+    }
+
+    public static bool TryParse(string str, out Version? version)
+    {
+        version = Parse(str);
+        return version is not null;
+    }
+
+
+    public override string ToString() => $"{Major}.{Minor}.{Patch}";
+}
