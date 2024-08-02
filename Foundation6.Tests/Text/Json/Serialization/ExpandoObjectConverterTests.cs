da5f832bd7a4cf4ddc72489973c93c0ecf7fa1b5
Markus Raufer <markus.raufer@gmail.com>
2024-08-02 22:49:42 +0200

    feat(Foundation): ExpandoObjectConverter

---
+++ Foundation6.Tests/Text/Json/Serialization/ExpandoObjectConverterTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Dynamic;
+using FluentAssertions;
+
+
+namespace Foundation.Text.Json.Serialization;
+
+[TestFixture]
+public class ExpandoObjectConverterTests
+{
+    [Test]
+    public void Test()
+    {
+        dynamic obj = new ExpandoObject();
+        obj.FirstName = "Markus";
+        obj.IsDirty = true;
+        obj.Alternatives = new string[] { "1", "2", "3" };
+        obj.State = new ExpandoObject();
+        obj.State.Name = "myName";
+        obj.State.Description = "my description";
+
+        var options = new JsonSerializerOptions
+        {
+            Converters = { new ExpandoObjectConverter() }
+        };
+
+        var json = JsonSerializer.Serialize<ExpandoObject>(obj, options) as string;
+
+        var expected = """{"FirstName":"Markus","IsDirty":true,"Alternatives":["1","2","3"],"State":{"Name":"myName","Description":"my description"}}""";
+        json.Should().Be(expected);
+    }
+}
---
+++ Foundation6/Text/Json/Serialization/ExpandoObjectConverter.cs
@@ -0,0 +1,33 @@
+﻿#if NET6_0_OR_GREATER
+using System;
+using System.Dynamic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Foundation.Text.Json.Serialization;
+public class ExpandoObjectConverter : JsonConverter<ExpandoObject>
+{
+    public override ExpandoObject? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Write(Utf8JsonWriter writer, ExpandoObject value, JsonSerializerOptions options)
+    {
+        WriteExpandoObject(writer, value, options);
+    }
+
+    private void WriteExpandoObject(Utf8JsonWriter writer, ExpandoObject obj, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+
+        foreach (var kvp in obj)
+        {
+            writer.WritePropertyName(kvp.Key);
+            if (kvp.Value is ExpandoObject xpando) WriteExpandoObject(writer, xpando, options);
+            else JsonSerializer.Serialize(writer, kvp.Value, options);
+        }
+        writer.WriteEndObject();
+    }
+}
+#endif
