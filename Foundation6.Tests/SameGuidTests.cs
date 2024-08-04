63e272d5a9b658a88a189f709779001e73569822
Markus Raufer <markus.raufer@gmail.com>
2024-08-04 12:31:34 +0200

    feat(Foundation): add SameGuid

---
+++ Foundation6.Tests/SameGuidTests.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foundation;
+
+[TestFixture]
+public class SameGuidTests
+{
+    [Test]
+    public void New_Should_Return_DifferntGuids_When_UsingDifferentIntSeedValues()
+    {
+        {
+            var guid1 = SameGuid.New(1);
+            var guid2 = SameGuid.New(2);
+            guid1.Should().NotBe(guid2);
+        }
+        {
+            var guid1 = SameGuid.New(11);
+            var guid2 = SameGuid.New(22);
+            guid1.Should().NotBe(guid2);
+        }
+    }
+
+    [Test]
+    public void New_Should_Return_DifferntGuids_When_UsingDifferentStringSeedValues()
+    {
+        var guid1 = SameGuid.New("apples");
+        var guid2 = SameGuid.New("oranges");
+        guid1.Should().NotBe(guid2);
+    }
+
+    [Test]
+    public void New_Should_Return_SameGuid_When_UsingSameIntSeed()
+    {
+        {
+            var guid1 = SameGuid.New(1);
+            var guid2 = SameGuid.New(1);
+            guid1.Should().Be(guid2);
+        }
+        {
+            var guid1 = SameGuid.New(11);
+            var guid2 = SameGuid.New(11);
+            guid1.Should().Be(guid2);
+        }
+    }
+
+    [Test]
+    public void New_Should_Return_SameGuid_When_UsingSameStringSeed()
+    {
+        var guid1 = SameGuid.New("apples");
+        var guid2 = SameGuid.New("apples");
+        guid1.Should().Be(guid2);
+    }
+}
---
+++ Foundation6/SameGuid.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Foundation;
+
+/// <summary>
+/// Creates the same Guid when using the same seed value.
+/// </summary>
+public static class SameGuid
+{
+    /// <summary>
+    /// Creates a same Guids if using same seed value.
+    /// This is needed for tests.
+    /// </summary>
+    /// <param name="seed">The value to create a Guid.</param>
+    /// <returns>A Guid value.</returns>
+    public static Guid New(object seed)
+    {
+#if NET6_0_OR_GREATER
+            byte[] hash = MD5.HashData(Encoding.Default.GetBytes($"{seed}"));
+#else
+        using MD5 md5 = MD5.Create();
+
+        byte[] hash = md5.ComputeHash(Encoding.Default.GetBytes($"{seed}"));
+#endif
+        return new Guid(hash);
+    }
+}
