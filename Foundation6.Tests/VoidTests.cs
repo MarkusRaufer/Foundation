97aa5ac7bd0f9b3007ff7e23e506b441595464c6
Markus Raufer <markus.raufer@gmail.com>
2024-09-11 19:31:32 +0200

    refactor(Foundation.Tests): Void.Returns

--- Foundation6.Tests/VoidTests.cs
+++ Foundation6.Tests/VoidTests.cs
@@ -1,10 +1,6 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Foundation;
 
@@ -12,12 +8,12 @@ namespace Foundation;
 public class VoidTests
 {
     [Test]
-    void Returns_Should_ReturnTrue_When_TrueIsSet()
+    public void Returns_Should_ReturnTrue_When_TrueIsSet()
     {
         List<int> items = [1, 2, 3];
         var index = 1;
 
-        var result = items.Count < index && Void.Returns(true, () => items.RemoveAt(index));
+        var result = items.Count > index && Void.Returns(true, () => items.RemoveAt(index));
 
         result.Should().BeTrue();
     }
