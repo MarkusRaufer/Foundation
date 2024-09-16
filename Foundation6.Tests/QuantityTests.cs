2f7050f44180d3a9d18dceb796330d1c06326947
Markus Raufer <markus.raufer@gmail.com>
2024-09-16 22:19:40 +0200

    feat(Foundation): add method Parse and TryParse to Quantity

---
+++ Foundation6.Tests/QuantityTests.cs
@@ -0,0 +1,98 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+
+namespace Foundation;
+
+[TestFixture]
+public class QuantityTests
+{
+    [Test]
+    public void Parse_Should_ReturnQuantity_When_NoGenericArgumentIsUsed()
+    {
+        //Arrange
+        var quantity = new Quantity("kg", 123.45M);
+        var str = quantity.ToString();
+
+        //Act
+        var sut = Quantity.Parse(str);
+
+        //Assert
+        sut.Should().Be(quantity);
+    }
+
+    [Test]
+    public void Parse_Should_ReturnQuantityWithValueOfTypeGuid_When_GenericArgumentIsGuid()
+    {
+        //Arrange
+        var guid = Guid.NewGuid();
+        var quantity = new Quantity<Guid>(nameof(Guid), guid);
+        var str = quantity.ToString();
+
+        //Act
+        var sut = Quantity.Parse<Guid>(str);
+
+        //Assert
+        sut.Should().Be(quantity);
+    }
+
+    [Test]
+    public void Parse_Should_ReturnQuantityWithValueOfTypeInt_When_GenericArgumentIsInt()
+    {
+        //Arrange
+        var quantity = new Quantity<int>("pcs", 123);
+        var str = quantity.ToString();
+
+        //Act
+        var sut = Quantity.Parse<int>(str);
+
+        //Assert
+        sut.Should().Be(quantity);
+    }
+
+    [Test]
+    public void TryParse_Should_ReturnTrue_When_NoGenericArgumentIsUsed()
+    {
+        //Arrange
+        var quantity = new Quantity("kg", 123.45M);
+        var str = quantity.ToString();
+
+        //Act
+        var success = Quantity.TryParse(str, out var sut);
+
+        //Assert
+        success.Should().BeTrue();
+        sut.Should().Be(quantity);
+    }
+
+    [Test]
+    public void TryParse_Should_ReturnQuantityWithValueOfTypeGuid_When_GenericArgumentIsGuid()
+    {
+        //Arrange
+        var guid = Guid.NewGuid();
+        var quantity = new Quantity<Guid>(nameof(Guid), guid);
+        var str = quantity.ToString();
+
+        //Act
+        var success = Quantity.TryParse<Guid>(str, out var sut);
+
+        //Assert
+        success.Should().BeTrue();
+        sut.Should().Be(quantity);
+    }
+
+    [Test]
+    public void TryParse_Should_ReturnQuantityWithValueOfTypeInt_When_GenericArgumentIsInt()
+    {
+        //Arrange
+        var quantity = new Quantity<int>("pcs", 123);
+        var str = quantity.ToString();
+
+        //Act
+        var success = Quantity.TryParse<int>(str, out var sut);
+
+        //Assert
+        success.Should().BeTrue();
+        sut.Should().Be(quantity);
+    }
+}
--- Foundation6/Quantity.cs
+++ Foundation6/Quantity.cs
@@ -21,7 +21,10 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-﻿namespace Foundation;
+using Foundation.Buffers;
+using System.Globalization;
+
+namespace Foundation;
 
 /// <summary>
 /// Type that represent a quantity.
@@ -37,6 +40,123 @@ public readonly record struct Quantity(string Unit, decimal Value)
     public static Quantity<TValue> New<TValue>(string unit, TValue value) => new(unit, value);
 
     public static Quantity<TUnit, TValue> New<TUnit, TValue>(TUnit unit, TValue value) => new(unit, value);
+
+    /// <summary>
+    /// Converts a string representation of a quantity to a <see cref="Quantity"/>.
+    /// </summary>
+    /// <param name="str">The string represents the string format when you call <see cref="ToString"/>.
+    /// </param>
+    /// <returns></returns>
+    public static Quantity Parse(string str)
+    {
+        return Parse(str, (string unit, decimal value) => new Quantity(unit, value));
+    }
+
+    /// <summary>
+    /// Converts a string representation of a quantity to a <see cref="Quantity{TValue}"/>.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the quantity value.</typeparam>
+    /// <param name="str">The string represents the string format when you call <see cref="ToString"/>.</param>
+    /// <returns></returns>
+    public static Quantity<TValue> Parse<TValue>(string str)
+    {
+        return Parse(str, (string unit, TValue value) => new Quantity<TValue>(unit, value));
+    }
+
+    private static TQuantity Parse<TValue, TQuantity>(string str, Func<string, TValue, TQuantity> factory)
+        where TQuantity : struct
+    {
+        var span = str.AsSpan();
+        var leftBraceIndex = span.IndexOf('{');
+        var rightBraceIndex = span.IndexOf('}');
+        var withoutBraces = span[(leftBraceIndex + 1)..(rightBraceIndex - 1)].Trim();
+
+        var splitter = new CharSplitEnumerator(withoutBraces, true, ',');
+        if (!splitter.MoveNext()) throw new InvalidArgumentException(nameof(str));
+
+        var unitTuple = splitter.Current.Trim();
+        var equalsSignIndex = unitTuple.IndexOf('=');
+        var unitValue = unitTuple[(equalsSignIndex + 1)..].Trim();
+        var unit = unitValue.ToString();
+
+        if (!splitter.MoveNext()) throw new InvalidArgumentException(nameof(str));
+
+        var valueTuple = splitter.Current.Trim();
+        equalsSignIndex = valueTuple.IndexOf('=');
+        var valueValue = valueTuple[(equalsSignIndex + 1)..].Trim();
+        var value = ParseValue<TValue>(valueValue.ToString());
+
+        return factory(unit, value);
+    }
+
+    /// <summary>
+    /// Converts a string representation of a quantity to a <see cref="Quantity"/>.
+    /// </summary>
+    /// <typeparam name="TValue">The value of the quantity.</typeparam>
+    /// <param name="str">In newer .NET version the string must be a JSON string.
+    /// In older .NET versions the string represents the string format when you call <see cref="ToString"/>.</param>
+    /// <param name="quantity">The converted <see cref="Quantity"/>.</param>
+    /// <returns></returns>
+    public static bool TryParse(string str, out Quantity quantity)
+    {
+        try
+        {
+            quantity = Parse(str);
+            return true;
+        }
+        catch (Exception)
+        {
+            quantity = default;
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Converts a string representation of a quantity to a <see cref="Quantity{TValue}"/>.
+    /// </summary>
+    /// <typeparam name="TValue">The value of the quantity.</typeparam>
+    /// <param name="str">In newer .NET version the string must be a JSON string.
+    /// In older .NET versions the string represents the string format when you call <see cref="ToString"/>.</param>
+    /// <param name="quantity">The converted <see cref="Quantity{TValue}"/></param>
+    /// <returns></returns>
+    public static bool TryParse<TValue>(string str, out Quantity<TValue> quantity)
+    {
+        try
+        {
+            quantity = Parse<TValue>(str);
+            return true;
+        }
+        catch (Exception)
+        {
+            quantity = default;
+            return false;
+        }
+    }
+
+    private static TValue ParseValue<TValue>(string valueStr)
+    {
+        Type type = typeof(TValue);
+        object value = type switch
+        {
+            _ when type == typeof(bool) => bool.Parse(valueStr),
+            _ when type == typeof(char) => char.Parse(valueStr),
+            _ when type == typeof(decimal) => decimal.Parse(valueStr, CultureInfo.InvariantCulture),
+            _ when type == typeof(double) => double.Parse(valueStr, CultureInfo.InvariantCulture),
+            _ when type == typeof(int) => int.Parse(valueStr, CultureInfo.InvariantCulture),
+            _ when type == typeof(long) => long.Parse(valueStr, CultureInfo.InvariantCulture),
+            _ when type == typeof(Guid) => Guid.Parse(valueStr),
+            _ when type == typeof(short) => short.Parse(valueStr, CultureInfo.InvariantCulture),
+            _ when type == typeof(string) => valueStr,
+            // Add more types as needed
+            _ => throw new NotSupportedException($"Type '{type.Name}' is not supported.")
+        };
+        return (TValue)value;
+    }
+
+    public override string ToString()
+    {
+        return $"Quantity {{ Unit = {Unit}, Value = {Value.ToString(CultureInfo.InvariantCulture)} }}";
+    }
 }
 
 /// <summary>
@@ -48,6 +168,12 @@ public readonly record struct Quantity(string Unit, decimal Value)
 public readonly record struct Quantity<TValue>(string Unit, TValue Value)
 {
     public readonly bool IsEmpty => 0 == GetHashCode();
+
+    public override string ToString()
+    {
+        var value = ((FormattableString)$"{Value}").ToString(CultureInfo.InvariantCulture);
+        return $"Quantity {{ Unit = {Unit}, Value = {value} }}";
+    }
 }
 
 /// <summary>
@@ -60,5 +186,11 @@ public readonly record struct Quantity<TValue>(string Unit, TValue Value)
 public readonly record struct Quantity<TUnit, TValue>(TUnit Unit, TValue Value)
 {
     public readonly bool IsEmpty => 0 == GetHashCode();
+
+    public override string ToString()
+    {
+        var value = ((FormattableString)$"{Value}").ToString(CultureInfo.InvariantCulture);
+        return $"Quantity {{ Unit = {Unit}, Value = {value} }}";
+    }
 }
 
