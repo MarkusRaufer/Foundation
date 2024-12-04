5f61c71964a3506a2a4d75875b4acd792da1efdf
Markus Raufer <markus.raufer@gmail.com>
2024-12-04 23:06:54 +0100

    refactor(Foundation.Tests): add ListExtensionsTests

---
+++ Foundation6.Tests/Collections/Generic/ListExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Foundation.Collections.Generic;
+
+[TestFixture]
+public class ListExtensionsTests
+{
+    [Test]
+    public void TryGet_Should_Return5_When_PredicateReturnsTrueOn5()
+    {
+        var items1 = Enumerable.Range(1, 10).ToList();
+
+        items1.TryGet(x => x == 5, out var value);
+        value.Should().Be(5);
+    }
+}
