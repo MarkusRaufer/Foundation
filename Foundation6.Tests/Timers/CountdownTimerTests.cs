db09d60ad654bad8a808d30aa2f86c32002ea64d
Markus Raufer <markus.raufer@gmail.com>
2025-04-08 01:05:28 +0200

    fea(Foundation.Timers)t: add CountdownTimer

---
+++ Foundation6.Tests/Timers/CountdownTimerTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Foundation.Timers;
+
+[TestFixture]
+public class CountdownTimerTests
+{
+    [Test]
+    public void IntervalElapsed_Should_BeCalledOnce_When_Subscribed()
+    {
+        var sut = new CountdownTimer(duration: TimeSpan.FromSeconds(2), interval: TimeSpan.FromSeconds(1));
+
+        var onElapsedCalled = 0;
+        
+        void onElapsed(TimeSpan span)
+        {
+            onElapsedCalled++;
+        }
+
+        var zero = TimeSpan.Zero;
+
+        sut.IntervalElapsed.Subscribe(onElapsed);
+        sut.Start();
+        
+        while (!sut.HasReachedZero)
+        {
+            Task.Delay(TimeSpan.FromSeconds(1));
+        }
+        onElapsedCalled.ShouldBe(1);
+    }
+
+    [Test]
+    public void ZeroReached_Should_BeCalledOnce_When_Subscribed()
+    {
+        var sut = new CountdownTimer(duration: TimeSpan.FromSeconds(1), interval: TimeSpan.FromSeconds(1));
+
+        var onElapsedCalled = 0;
+
+        void onIntervalElapsed(TimeSpan span)
+        {
+            onElapsedCalled++;
+        }
+
+        var onZeroReachedCalled = 0;
+
+        void onZeroReached()
+        {
+            onZeroReachedCalled++;
+        }
+
+        var zero = TimeSpan.Zero;
+
+        sut.IntervalElapsed.Subscribe(onIntervalElapsed);
+        sut.ZeroReached.Subscribe(onZeroReached);
+
+        sut.Start();
+
+        while (!sut.HasReachedZero)
+        {
+            Task.Delay(TimeSpan.FromSeconds(1));
+        }
+        onElapsedCalled.ShouldBe(1);
+        onZeroReachedCalled.ShouldBe(1);
+    }
+}
---
+++ Foundation6/Timers/CountdownTimer.cs
@@ -0,0 +1,68 @@
+﻿using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace Foundation.Timers;
+
+public class CountdownTimer
+{
+    public Event<Action<TimeSpan>> IntervalElapsed = new();
+    public Event<Action> ZeroReached = new();
+
+    private readonly Timer _timer;
+
+    public CountdownTimer(TimeSpan duration, TimeSpan interval)
+    {
+        Duration = duration;
+        RemainingDuration = duration;
+        Interval = interval;
+
+        _timer = new Timer(interval.TotalMilliseconds);
+        _timer.Elapsed += OnTimerIntervalElapsed;
+        _timer.AutoReset = true;
+        _timer.Enabled = true;
+
+        HasReachedZero = false;
+    }
+
+    public CountdownTimer(Timer timer, TimeSpan duration)
+    {
+        _timer = timer.ThrowIfNull();
+        Duration = duration;
+        RemainingDuration = duration;
+
+        _timer.Elapsed += OnTimerIntervalElapsed;
+        _timer.AutoReset = true;
+        _timer.Enabled = true;
+
+        Interval = TimeSpan.FromMilliseconds(_timer.Interval);
+
+        HasReachedZero = false;
+    }
+
+    public TimeSpan Duration { get; }
+
+    public bool HasReachedZero { get; private set; }
+
+    public TimeSpan RemainingDuration { get; private set; }
+
+    public TimeSpan Interval { get; }
+
+    private void OnTimerIntervalElapsed(object? sender, ElapsedEventArgs e)
+    {
+        RemainingDuration = RemainingDuration.Subtract(Interval);
+        
+        if (RemainingDuration.Ticks <= 0)
+        {
+            _timer.Stop();
+            RemainingDuration = TimeSpan.Zero;
+            HasReachedZero = true;
+            ZeroReached.Publish();
+        }
+
+        IntervalElapsed.Publish(RemainingDuration);
+    }
+
+    public void Start() => _timer.Start();
+
+    public void Stop() => _timer.Stop();
+}
