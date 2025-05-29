d7b7c79b6060510ef35c4cb1f5579b70a58993e8
Markus Raufer <markus.raufer@gmail.com>
2025-05-29 21:05:26 +0200

    refactor: TupleExtensions

--- Foundation6/Collections/Generic/TupleExtensions.cs
+++ Foundation6/Collections/Generic/TupleExtensions.cs
@@ -25,6 +25,9 @@
 
 public static class TupleExtensions
 {
+    public static KeyValuePair<TKey, TValue> ToKeyValue<TKey, TValue>(this (TKey, TValue) tuple)
+        => new(tuple.Item1, tuple.Item2);
+
     public static (T[], T[]) ToArrays<T>(this (IEnumerable<T>, IEnumerable<T>) tuple)
     {
         return (tuple.Item1.ToArray(), tuple.Item2.ToArray());
